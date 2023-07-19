6ff0516defecb4af1b70b64e6c4045709093564c
1kuzus <iku2u5@outlook.com>
2023-07-19 23:58:17 +0800

    init

--- src/component/Homepage/Homepage.js
+++ src/component/Homepage/Homepage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Homepage.css';
+import {useNavigate} from 'react-router-dom'
 
 export default function Homepage() {
+    const navigate = useNavigate();
     return (
         <>
+        <button onClick={()=>navigate('/list')}>to list</button>
             {/* <div className="homepage">Homepage</div>
             <div className="mask"></div>
             <div className="header"></div> */}
