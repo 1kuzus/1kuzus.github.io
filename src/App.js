049cd536c0c7d5bb42826175ded8727c1673bbf3
1kuzus <iku2u5@outlook.com>
2023-07-19 23:55:12 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import Homepage from './component/Homepage/Homepage';
 import Testblog from './component/Testblog/Testblog';
 import Header from './component/Header/Header';
+
+import {useRoutes, BrowserRouter} from 'react-router-dom';
 import './App.css';
 
+const router = [
+    {
+        path: '/',
+        element: <Homepage />,
+    },
+    {
+        path: '/list',
+        element: <Testblog />,
+    },
+    // {
+    //     path: '/',
+    //     element: <Layout />,
+    //     children: [
+    //         {
+    //             path: 'dashboard',
+    //             element: withLoadingComponent(<Dashboard />),
+    //         },
+    //         {
+    //             path: 'content',
+    //             element: withLoadingComponent(<Content />),
+    //         },
+    //     ],
+    // },
+    // {
+    //     path: '*',
+    //     element: <Navigate to="/dashboard" />,
+    // },
+];
+
 function App() {
-    return (
-        <div className="App">
-            {/* <Homepage></Homepage> */}
-            <Testblog></Testblog>
-            <Header></Header>
-        </div>
-    );
+    return <>{useRoutes(router)}</>;
 }
 
 export default App;
--- src/component/Homepage/Homepage.js
+++ src/component/Homepage/Homepage.js
@@ -4,9 +4,9 @@ import './Homepage.css';
 export default function Homepage() {
     return (
         <>
-            <div className="homepage">Homepage</div>
+            {/* <div className="homepage">Homepage</div>
             <div className="mask"></div>
-            <div className="header"></div>
+            <div className="header"></div> */}
         </>
     );
 }
