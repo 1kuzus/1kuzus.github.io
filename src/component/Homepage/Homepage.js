935dad2b6dd78f13144dddaf2cc79ed9f8a5454d
1kuzus <iku2u5@outlook.com>
2023-07-23 00:55:17 +0800

    init

--- src/component/Homepage/Homepage.js
+++ src/component/Homepage/Homepage.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Homepage.css';
 import {useNavigate} from 'react-router-dom';
 
 export default function Homepage() {
     const navigate = useNavigate();
+    const [logoExpand, setLogoExpand] = useState(true);
     return (
         <div id="homepage">
             <div id="homepage-content">
-                <div id="logopart-wrapper">
+                <div
+                    id="logopart-wrapper"
+                    onMouseEnter={() => setLogoExpand(!logoExpand)}
+                    className={logoExpand && 'logo-expand'}
+                >
                     <div id="logopart-x"></div>
                     <div id="logopart-y"></div>
                     <div id="logopart-vl"></div>
                     <div id="logopart-vr"></div>
                 </div>
-                <h1 id="homepage-title">Suzuki Blog</h1>
+                <h1 id="homepage-title">Blog-1kuzus</h1>
             </div>
             <ul>
                 <div>list1</div>
                 <li onClick={() => navigate('/blogname')}>blog1</li>
-                <li onClick={() => navigate('/blogname2')}>blog2</li>
+                <li onClick={() => navigate('/blogname2')}>blog2</li>W
             </ul>
             <ul>
                 <div>list2</div>
