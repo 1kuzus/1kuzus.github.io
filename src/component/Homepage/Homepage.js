fb536195e1f20acb7758c8cd41f9e261b41a61df
1kuzus <iku2u5@outlook.com>
2023-07-23 16:57:54 +0800

    init

--- src/component/Homepage/Homepage.js
+++ src/component/Homepage/Homepage.js
@@ -18,7 +18,7 @@ export default function Homepage() {
                     <div id="logopart-vl"></div>
                     <div id="logopart-vr"></div>
                 </div>
-                <h1 id="homepage-title">Write anything here</h1>
+                <h1 id="homepage-title">Suzuki</h1>
             </div>
             <ul>
                 <div>list1</div>
