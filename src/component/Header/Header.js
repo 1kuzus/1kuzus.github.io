a8ff16db6fb7b2a4d4a44a22feea985492c512ff
1kuzus <iku2u5@outlook.com>
2023-07-23 21:29:41 +0800

    init

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LogoIcon from '../../assets/logo.svg';
+import GithubIcon from '../../assets/github.svg';
 import './Header.css';
 
 export default function Header() {
     return (
         <div id="header">
-            <img src={LogoIcon} />
+            <img src={LogoIcon} id="header-logo" />
+            <img src={GithubIcon} id="header-github" />
         </div>
     );
 }
