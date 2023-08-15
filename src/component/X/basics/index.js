3db521b19bb15ce61325a90c28c1d1b2169c0b4b
1kuzus <revenge.1kuzus@outlook.com>
2023-08-16 00:34:20 +0800

    init

--- src/component/X/basics/index.js
+++ src/component/X/basics/index.js
@@ -24,7 +24,15 @@ export function P(props) {
     */
     const {children} = props;
     let htmlContent = Array.isArray(children) ? children.join('') : children;
-    htmlContent = htmlContent.replace(/</g, '&#60;\n').replace(/>/g, '&#62;');
+    htmlContent = htmlContent.replace(/</g, '&#60;').replace(/>/g, '&#62;');
+    htmlContent = htmlContent.replace(/\\\\/g, '&#92;');
+    htmlContent = htmlContent.replace(/\\`/g, '&#96;');
+    htmlContent = htmlContent.replace(/\\\*/g, '&#42;');
+    htmlContent = htmlContent.replace(/\\@/g, '&#64;');
+    htmlContent = htmlContent.replace(/\\n/g, '<br/>');
+    htmlContent = htmlContent.replace(/`(.*?)`/g, '<span class="x-inline-highlight">$1</span>');
+    htmlContent = htmlContent.replace(/\*(.*?)\*/g, '<span class="x-inline-strong">$1</span>');
+    htmlContent = htmlContent.replace(/@(.*?)\[(.*?)\]@/g, '<a href="$2" target="_blank" class="x-inline-link">$1</a>');
     // const styled = children.replace(/`(.*?)`/g, '<span class="x-inline-highlight">$1</span>');
     console.log(children);
     console.log(htmlContent);
