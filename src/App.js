e07ee56a94b491b1fbe2b002bc88a757e3f1cce3
1kuzus <revenge.1kuzus@outlook.com>
2023-08-21 21:21:28 +0800

    init

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {useEffect} from 'react';
 import hljs from 'highlight.js';
 import './App.css';
 
-import 'highlight.js/styles/vs2015.css';
+// import 'highlight.js/styles/vs2015.css';
 import Blog1 from './blogs/b1-blogname';
 import Blog2 from './blogs/b2-blogname2';
 import SideBar from './component/SideBar/SideBar';
--- src/component/X/CodeBlock/CodeBlock.js
+++ src/component/X/CodeBlock/CodeBlock.js
@@ -2,10 +2,14 @@ import './CodeBlock.css';
 
 export default function CodeBlock(props) {
     const {language, code} = props;
+    let lines = code.split('\n').map((line) => line.trimRight());
+    if (!lines[0]) lines = lines.slice(1);
+    const indent = lines[0].length - lines[0].trimStart().length;
+    lines = lines.map((line) => line.slice(indent));
     return (
         <div className="x-codeblock">
             <pre>
-                <code className={`lang-${language}`}>{code}</code>
+                <code className={`lang-${language}`}>{lines.join('\n')}</code>
             </pre>
         </div>
     );
