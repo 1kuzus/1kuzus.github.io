c785361f33eb545313c133133f67592ba56ccc59
1kuzus <iikuzus@outlook.com>
2023-08-29 00:01:26 +0800

    init

--- src/component/Homepage/Homepage.js
+++ src/component/Homepage/Homepage.js
@@ -1,24 +1,17 @@
-import {useState} from 'react';
 import BlogLists from '../BlogLists/BlogLists';
 import './Homepage.css';
 
 export default function Homepage() {
-    const [logoExpand, setLogoExpand] = useState(false);
     return (
         <div id="homepage">
             <div id="homepage-content">
-                <div
-                    id="logopart-wrapper"
-                    onMouseEnter={() => setLogoExpand(true)}
-                    onMouseLeave={() => setLogoExpand(false)}
-                    className={logoExpand ? 'logo-expand' : null}
-                >
+                <div id="logopart-wrapper">
                     <div id="logopart-x" />
                     <div id="logopart-y" />
                     <div id="logopart-vl" />
                     <div id="logopart-vr" />
                 </div>
-                {/* <h1 id="homepage-title">铃木のBlogs</h1> */}
+                <h1 id="homepage-title">铃木のBlogs</h1>
             </div>
             <BlogLists />
         </div>
--- src/component/SideBar/SideBar.js
+++ src/component/SideBar/SideBar.js
@@ -1,11 +1,11 @@
 import {useState} from 'react';
-import {useNavigate, useLocation} from 'react-router-dom';
+import {useLocation, Link} from 'react-router-dom';
 import categories from '../../blogs/categories';
 import RightArrowIcon from '../../assets/rightarrow.svg';
 import './SideBar.css';
 
 function SideBarList(props) {
-    const {category, currentPath, navigate} = props;
+    const {category, currentPath} = props;
     const [showList, setShowList] = useState(true);
     return (
         <div className={`sidebarlist${showList ? ' showlist' : ''}`}>
@@ -15,13 +15,10 @@ function SideBarList(props) {
             </div>
             <ul className="sidebarlist-ul" style={{height: showList ? 34 * category.blogs.length + 'px' : '0'}}>
                 {category.blogs.map((blog, index) => (
-                    //todo:用a标签改造
-                    <li
-                        className={`sidebarlist-li${currentPath === blog.path ? ' active' : ''}`}
-                        key={index}
-                        onClick={() => navigate(blog.path)}
-                    >
-                        {blog.blogName}
+                    <li key={index}>
+                        <Link className={`sidebarlist-li${currentPath === blog.path ? ' active' : ''}`} to={blog.path}>
+                            {blog.blogName}
+                        </Link>
                     </li>
                 ))}
             </ul>
@@ -31,11 +28,10 @@ function SideBarList(props) {
 
 export default function SideBar() {
     const location = useLocation();
-    const navigate = useNavigate();
     return (
         <div id="sidebar">
             {categories.map((category, index) => (
-                <SideBarList key={index} category={category} currentPath={location.pathname} navigate={navigate} />
+                <SideBarList key={index} category={category} currentPath={location.pathname} />
             ))}
         </div>
     );
