1cd0632b954f8cc9de3c9d39f6db66f7b2f982a5
1kuzus <revenge.1kuzus@outlook.com>
2023-08-29 13:53:50 +0800

    init

--- src/component/X/BlogWrapper/BlogWrapper.js
+++ src/component/X/BlogWrapper/BlogWrapper.js
@@ -1,14 +1,18 @@
 import {useLayoutEffect} from 'react';
 import 'prismjs/themes/prism.css';
 import hljs from 'highlight.js';
-import prismjs from 'prismjs';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-python';
 import './BlogWrapper.css';
 
+// Prism.manual = true;
+
+Prism.highlightAll();
 
 export default function BlogWrapper(props) {
     const {children} = props;
     useLayoutEffect(() => {
-        prismjs.highlightAll();
+        Prism.highlightAll();
     }, []);
     return <div className="x-blogwrapper">{children}</div>;
 }
