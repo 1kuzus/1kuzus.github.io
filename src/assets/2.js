05002c31129763e0fde94fc1a41e39061fd4306d
1kuzus <revenge.1kuzus@outlook.com>
2023-08-31 22:20:52 +0800

    init

--- src/assets/2.js
+++
@@ -1,9 +0,0 @@
-const test = fmt`123`;
-function fmt(strings) {
-    const aa = [/21132123/];
-    const bb = strings;
-    console.log(strings);
-    console.log(String.raw(bb)); // 正确
-    console.log(String.raw(...aa)); // 正确，注意这里使用了 ... 拓展运算符
-    return String.raw(strings);
-}
--- src/blogs/2308/pr1/index.js
+++ src/blogs/2308/pr1/index.js
@@ -7,10 +7,15 @@ export default function Blog() {
             <X.Title>「模式识别」统计决策方法</X.Title>
 
             <X.H2>贝叶斯公式</X.H2>
+            <X.P>考虑如下例子，记A为`抓到方形`，B为`抓到实心图形`：</X.P>
             <div className="behwl-boxes">
-                <div className="behwl-square" />
-                <div className="behwl-square" />
+                <div className="behwl-box behwl-fill" />
+                <div className="behwl-box" />
+                <div className="behwl-box" />
+                <div className="behwl-box behwl-circle behwl-fill" />
+                <div className="behwl-box" />
             </div>
+            <X.Formula text="P(A) = \frac{3}{5}" />
             <X.Formula text="P(AB) = \frac{P(AB)}{P(B)} = \frac{P(B|A) \cdot P(A)}{P(B)}" />
             <X.Formula text="P(A|B) = \frac{P(AB)}{P(B)} = \frac{P(B|A) \cdot P(A)}{P(B)}" />
 
