45e640b94f036d56d33224442f1aea32165a1cc9
1kuzus <revenge.1kuzus@outlook.com>
2023-08-31 14:00:50 +0800

    init

---
+++ src/blogs/2308/pr1.js
@@ -0,0 +1,15 @@
+import X from '../../component/X';
+
+export default function Blog() {
+    return (
+        <X.BlogWrapper>
+            <X.Title>「模式识别」统计决策方法</X.Title>
+
+            <X.H2>贝叶斯公式</X.H2>
+            <X.Formula text="P(AB) = \frac{P(AB)}{P(B)} = \frac{P(B|A) \cdot P(A)}{P(B)}" />
+            <X.Formula text="P(A|B) = \frac{P(AB)}{P(B)} = \frac{P(B|A) \cdot P(A)}{P(B)}" />
+
+            <X.H2>最小错误率贝叶斯决策</X.H2>
+        </X.BlogWrapper>
+    );
+}
