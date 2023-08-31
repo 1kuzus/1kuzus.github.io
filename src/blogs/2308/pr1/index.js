34c23687c7a9813a69356af22477e5f79b966123
1kuzus <revenge.1kuzus@outlook.com>
2023-08-31 14:26:07 +0800

    init

--- src/blogs/2308/pr1/index.js
+++ src/blogs/2308/pr1/index.js
@@ -1,4 +1,4 @@
-import X from '../../component/X';
+import X from 'src/component/X';
 
 export default function Blog() {
     return (
@@ -6,6 +6,9 @@ export default function Blog() {
             <X.Title>「模式识别」统计决策方法</X.Title>
 
             <X.H2>贝叶斯公式</X.H2>
+            <div className="behwl-boxes">
+                <div className="behwl-square" />
+            </div>
             <X.Formula text="P(AB) = \frac{P(AB)}{P(B)} = \frac{P(B|A) \cdot P(A)}{P(B)}" />
             <X.Formula text="P(A|B) = \frac{P(AB)}{P(B)} = \frac{P(B|A) \cdot P(A)}{P(B)}" />
 
