3a0f2f9d2e556db10b580281fe235f6e956f62da
1kuzus <revenge.1kuzus@outlook.com>
2023-09-02 17:09:32 +0800

    init

--- src/component/SideBar/SideBar.js
+++ src/component/SideBar/SideBar.js
@@ -1,7 +1,6 @@
 import {useState} from 'react';
 import {useLocation, Link} from 'react-router-dom';
 import categories from '@/blogs/categories';
-import RightArrowIcon from '@/assets/rightarrow.svg';
 import './SideBar.css';
 
 function SideBarList(props) {
@@ -11,7 +10,16 @@ function SideBarList(props) {
         <div className={`sidebarlist${showList ? ' showlist' : ''}`}>
             <div className="sidebarlist-head" onClick={() => setShowList(!showList)}>
                 <h3 className="sidebarlist-title">{category.categoryName + `  (${category.blogs.length})`}</h3>
-                <img className="sidebarlist-title-rightarrow" src={RightArrowIcon} />
+                <svg
+                    className="sidebarlist-title-rightarrow"
+                    viewBox="0 0 1024 1024"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M761.056 532.128c.512-.992 1.344-1.824 1.792-2.848 8.8-18.304 5.92-40.704-9.664-55.424L399.936 139.744c-19.264-18.208-49.632-17.344-67.872 1.888-18.208 19.264-17.376 49.632 1.888 67.872l316.96 299.84L335.2 813.632c-19.072 18.4-19.648 48.768-1.248 67.872 9.408 9.792 21.984 14.688 34.56 14.688 12 0 24-4.48 33.312-13.44l350.048-337.376c.672-.672.928-1.6 1.6-2.304.512-.48 1.056-.832 1.568-1.344 2.72-2.848 4.16-6.336 6.016-9.6z"
+                        fill="#50505c"
+                    />
+                </svg>
             </div>
             <ul className="sidebarlist-ul" style={{height: showList ? 34 * category.blogs.length + 'px' : '0'}}>
                 {category.blogs.map((blog, index) => (
@@ -28,7 +36,6 @@ function SideBarList(props) {
 
 export default function SideBar() {
     const location = useLocation();
-
     return (
         <div id="sidebar">
             {categories.map((category, index) => (
--- src/component/X/Formula/Formula.js
+++ src/component/X/Formula/Formula.js
@@ -4,12 +4,14 @@ import {useLayoutEffect, useRef} from 'react';
 export default function Formula(props) {
     const elementRef = useRef();
     const {text} = props;
+
     useLayoutEffect(() => {
         Katex.render(text, elementRef.current, {
             output: 'html',
             strict: false,
         });
     }, [text]);
+
     return (
         <div className="x-formula" ref={elementRef}>
             {text}
