0ef54ffdcd20d730835a0f308c30de6ddceb9453
1kuzus <iikuzus@outlook.com>
2023-09-05 15:24:04 +0800

    init

--- src/blogs/23c/pr1/index.js
+++ src/blogs/23c/pr1/index.js
@@ -20,7 +20,7 @@ export default function Blog() {
                 <div className="behwl-box" />
                 <div className="behwl-box" />
                 <div className="behwl-box behwl-circle behwl-fill" />
-                <div className="behwl-box" />
+                <div className="behwl-box behwl-circle" />
             </div>
             <X.Uli>
                 <X.Formula className=".x-text-active" text="P(A) = \frac{3}{5} \quad P(B) = \frac{2}{5}" />
@@ -61,6 +61,11 @@ export default function Blog() {
                 <X.P>`对数似然比`</X.P>
                 <X.Formula text="h(x)=-ln[l(x)] \ 与 \ ln\frac{P(\omega_1)}{P(\omega_2)} \quad 比较，h(x)小选\omega_1" />
             </X.Oli>
+            <X.HighlightBlock>
+                <X.P>
+                    可以把每一类的后验概率`P(ωi|x)`或者`P(x|ωi)P(ωi)`看作该类的一个`判别函数g(x)`，决策的过程就是各类的判别函数比较大小
+                </X.P>
+            </X.HighlightBlock>
 
             <X.H1>最小风险贝叶斯决策</X.H1>
             <X.H2>条件风险</X.H2>
@@ -94,7 +99,7 @@ export default function Blog() {
             <X.H1>正态分布的统计决策</X.H1>
             <X.H2>从一维正态分布到高维正态分布</X.H2>
             <X.P>单变量正态分布函数：</X.P>
-            <X.Formula text="p(x) = \frac{1}{\sqrt{2\pi}\sigma} exp\{-\frac{1}{2} \cdot (\frac{x-\mu}{\sigma})^2\}" />
+            <X.Formula text="p(x) = \frac{1}{\sqrt{2\pi}\sigma} \exp\{-\frac{1}{2} \cdot (\frac{x-\mu}{\sigma})^2\}" />
             <X.P>双变量正态分布函数：</X.P>
             <X.Formula
                 text="p(x_1,x_2) = \frac{1}{2\pi\sigma_1\sigma_2\sqrt{1-\rho^2}} \exp\{
@@ -119,15 +124,81 @@ export default function Blog() {
                 \bm{\Sigma}=
                 \begin{bmatrix}
                 \sigma_1^2 & \rho\sigma_1\sigma_2 \\
-                \rho\sigma_1\sigma_2 & \sigma_2^2 \\
+                \rho\sigma_2\sigma_1 & \sigma_2^2 \\
                 \end{bmatrix}
                 "
             />
             <X.P>则用矩阵形式表示为：</X.P>
             <X.HighlightBlock>
-                <X.Formula text="p(\bm{X}) = \frac{1}{(2\pi)^{\frac{d}{2}}}"/>
-                <X.Formula text="\frac{1}{1+\frac{1}{1+\frac{1}{1+\frac{1}{1+1}}}}"/>
+                <X.Formula
+                    text="p(\bm{X}) = \frac{1}{(2\pi)^{\frac{d}{2}}|\bm{\Sigma}|^\frac{1}{2}} \exp \{
+                    -\frac{1}{2} \cdot (\bm{X}-\bm{\mu})^T \bm{\Sigma}^{-1} (\bm{X}-\bm{\mu}) 
+                    \}
+                    "
+                />
+                <X.P>\n此式通用于高维正态分布。</X.P>
             </X.HighlightBlock>
+            <X.P>
+                以上公式很长，从两个概念入手：`Σ`矩阵代表什么？`ρ`似乎没有在通式中体现，其含义是什么？\n
+                对于二维正态分布例子中的两个变量来说，他们的*协方差*定义为：
+            </X.P>
+            <X.Formula text="cov(x_1,x_2)=E[(x_1-\mu_1)(x_2-\mu_2)]=E(x_1x_2)-\mu_1\mu_2" />
+            <X.P>`Σ`矩阵的含义其实是两个变量的协方差矩阵，也就是：</X.P>
+            <X.Formula
+                text="\bm{\Sigma} =
+                \begin{bmatrix}
+                cov(x_1,x_1) & cov(x_1,x_2) \\
+                cov(x_2,x_1) & cov(x_2,x_2)
+                \end{bmatrix}，其中cov(x_1,x_1)=\sigma_1^2
+                "
+            />
+            <X.P>`ρ`的含义为两个变量的相关系数，以上述变量`x1`,`x2`为例，计算公式为：</X.P>
+            <X.Formula text="\rho = \frac{cov(x_1,x_2)}{\sigma_1\sigma_2} \Rightarrow cov(x_1,x_2)=\rho\sigma_1\sigma_2" />
+            <X.P>至此就可以理解`Σ`,`ρ`两个参数的含义了！</X.P>
+            <X.H2>正态分布概率模型下的最小错误率贝叶斯决策</X.H2>
+            <X.Formula
+                text="P(\bm{X}|\omega_i) = \frac{1}{(2\pi)^{\frac{d}{2}}|\bm{\Sigma_i}|^\frac{1}{2}} \exp \{
+                -\frac{1}{2} \cdot (\bm{X}-\bm{\mu_i})^T \bm{\Sigma_i}^{-1} (\bm{X}-\bm{\mu_i}) 
+                \}
+                "
+            />
+            <X.P>
+                注意这个类概率密度函数`P(X|ωi)`的含义为，第`ωi`类的样本`X`的概率密度。\n使用后验概率的变形作为判别函数：
+            </X.P>
+            <X.Formula
+                text="g_i(\bm{X}) = ln[P(\bm{X}|\omega_i)P(\omega_i)] =
+                -\frac{d}{2}ln2\pi -\frac{1}{2}ln|\bm{\Sigma_i}| -\frac{1}{2}(\bm{X}-\bm{\mu_i})^T\bm{\Sigma_i}^{-1}(\bm{X}-\bm{\mu_i}) +lnP(\omega_i)
+                "
+            />
+            <X.P>从以下三种情况考虑决策面：</X.P>
+            <X.Oli reset>
+                <X.Formula text="\bm{\Sigma_1} = \bm{\Sigma_2}=...=\bm{\Sigma_c} = \sigma^2\bm{I}" />
+            </X.Oli>
+            <X.P>
+                各类模式分布的协方差矩阵相等，样本统计独立且方差相同，协方差均为0。\n此时`g(X)`的前两项与类别无关，后两项化简为：
+            </X.P>
+            <X.Formula text="g_i(\bm{X}) = -\frac{1}{2\sigma^2}\Vert\bm{X}-\bm{\mu_i}\Vert^2 +lnP(\omega_i)" />
+            <X.HighlightBlock>
+                <X.P>如果先验概率相等，则决策只与欧氏距离有关。此时决策为：</X.P>
+                <X.Br></X.Br>
+                <X.Formula text="若\Vert\bm{X}-\bm{\mu_i}\Vert^2 = min_{j=1,...,c} \Vert\bm{X}-\bm{\mu_j}\Vert^2，则x属于\omega_i类" />
+            </X.HighlightBlock>
+            <X.HighlightBlock bgcolor="gray">
+                <X.P>
+                    从几何的视角来看，以上决策规则实际就是比较样本点和各类的中心点距离，并且选择距离最近的类别作为决策结果。\n
+                    以上分类器也称`最小距离分类器`，把每个`均值`看作一个`典型的样本`，则这种分类方法也称为`模板匹配技术`。
+                </X.P>
+            </X.HighlightBlock>
+            <X.P>如果对于上述判别函数`g(X)`的欧式距离项展开，并删掉与类别无关的二次项，得：</X.P>
+            <X.Formula text="g_i(\bm{X}) = \bm{W_i}^T\bm{X} + b，其中\bm{W_i} = \frac{1}{\sigma^2}\bm{\mu_i}，b = -\frac{1}{2\sigma^2}\bm{\mu_i}^T\bm{\mu_i} + lnP(\omega_i)" />
+            <X.P>判别函数是`X`的线性函数，称为`线性分类器`。</X.P>
+
+            <X.Oli>
+                <X.Formula text="\bm{\Sigma_1} = \bm{\Sigma_2}=...=\bm{\Sigma_c} = \bm{\Sigma}" />
+            </X.Oli>
+            <X.Oli>
+                <X.Formula text="\bm{\Sigma_i} \neq \bm{\Sigma_j}" />
+            </X.Oli>
         </X.BlogWrapper>
     );
 }
