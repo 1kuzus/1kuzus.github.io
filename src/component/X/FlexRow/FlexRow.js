9f51d3f5da4d0be390d1551d1ec980ab3171c389
1kuzus <iikuzus@outlook.com>
2023-09-06 20:37:28 +0800

    init

--- src/component/X/FlexRow/FlexRow.js
+++ src/component/X/FlexRow/FlexRow.js
@@ -1,9 +1,9 @@
 import './FlexRow.css';
 
 export default function FlexRow(props) {
-    const {children, width} = props;
+    const {children, width, gap} = props;
     return (
-        <div className="x-flexrow" style={{width}}>
+        <div className="x-flexrow" style={{width, gap}}>
             {children}
         </div>
     );
