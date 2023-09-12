bb2741daa8c3cdbf3a038744bf4d3a58dc0b3bd2
1kuzus <iikuzus@outlook.com>
2023-09-12 23:22:48 +0800

    init

--- src/utils/test.js
+++ src/utils/test.js
@@ -1,5 +1,4 @@
+const arr = [1, 2, 3];
 
-                const arr = [0, 1, 2, 3, 2];
-                console.log(arr.lastIndexOf(2)); //4
-                console.log(arr.lastIndexOf(2, 2)); //2
-                console.log(arr.lastIndexOf(9)); //-1
+arr.forEach(i=>i*2)
+console.log(arr)
