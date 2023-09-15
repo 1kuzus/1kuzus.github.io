0cacaa2e9c5d28bea248b8c0eda6f36fac3ff4ee
1kuzus <iikuzus@outlook.com>
2023-09-15 23:05:32 +0800

    init

--- src/blogs/23c/pr2/index.js
+++ src/blogs/23c/pr2/index.js
@@ -3,37 +3,17 @@ import X from '@/component/X';
 export default function Blog() {
     return (
         <X.BlogWrapper>
-            21345
-            {/* <X.Title>「模式识别」概率密度函数的估计</X.Title> */}
-            <X.Title>识概憚计</X.Title>
+            <X.Title>「模式识别」概率密度函数的估计</X.Title>
             <X.P>
-                上一篇提到的统计决策方法，认为类条件密度已知。但实际问题中，往往只有已知样本，需要根据已知$\mu_1$推测分布。
+                上一篇提到的统计决策方法，类条件密度按已知处理。但实际问题中，往往只有已知样本，需要根据已知样本推测分布。
             </X.P>
+
+            <X.H1>最大似然估计</X.H1>
             <X.P>
-                上一篇提到的统计决策方法，认为类条件密度已知。但实际问题中，往往\n 只有已知样本，需要根据已知
-                {`$\\int_{a}^{b} f(x)dx = \\lim_{n \\to \\infty} \\sum_{i=1}^{n} f(x_i) \\cdot \\Delta x$样本$\\mu_8$`}
-                推测分布。
+                假设样本的分布形式已知，现在想确定参数{'$\\bm{\\theta}$'}。如果已经观测到了一些样本，记这些样本为
+                {'$\\bm{\\chi}$'}，则我们要找的参数即为使得出现观测样本{'$\\bm{\\chi}$'}概率最大的参数
+                {'$\\bm{\\theta}$'}。
             </X.P>
-            {/* <X.H1>最大似然估计</X.H1> */}
-            <X.HighlightBlock>
-                123456
-                <X.Oli>
-                    olifather
-                    <X.Oli>456</X.Oli>
-                    <X.Oli>987</X.Oli>
-                </X.Oli>
-                <X.Oli>12</X.Oli>
-                <X.Oli>12</X.Oli>
-                <X.HighlightBlock>
-                    <X.P>13 `456` @baidu[https://www.baidu.com]@</X.P>
-                </X.HighlightBlock>
-                <X.Formula text="123+456=579"></X.Formula>
-            </X.HighlightBlock>
-            <X.HighlightBlock>
-                <X.P>123</X.P>
-            </X.HighlightBlock>
-            <X.Formula text="123+456=579"></X.Formula>
-            <X.P>$123+456=579$也是$\sigma_1$</X.P>
         </X.BlogWrapper>
     );
 }
