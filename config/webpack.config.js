2341e7cd37f6eeff2a8f644e9254e662a4641f19
1kuzus <iikuzus@outlook.com>
2023-09-17 19:26:34 +0800

    init

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,3 @@
-'use strict';
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
@@ -653,6 +652,7 @@ module.exports = function (webpackEnv) {
                                 declarationMap: false,
                                 noEmit: true,
                                 incremental: true,
+                                jsx: 'react-jsx',
                                 tsBuildInfoFile: paths.appTsBuildInfoFile,
                             },
                         },
--- src/blogs/23c/pr2/index.js
+++ src/blogs/23c/pr2/index.js
@@ -11,9 +11,19 @@ export default function Blog() {
             <X.H1>最大似然估计</X.H1>
             <X.P>
                 假设样本的分布形式已知，现在想确定参数{'$\\bm{\\theta}$'}。如果已经观测到了一些样本，记这些样本为
-                {'$\\bm{\\chi}$'}，则我们要找的参数即为使得出现观测样本{'$\\bm{\\chi}$'}概率最大的参数
+                {'$\\bm{\\chi}$'}，则我们要找的参数即为：使得出现观测样本{'$\\bm{\\chi}$'}概率最大的参数
                 {'$\\bm{\\theta}$'}。
             </X.P>
+            <X.P>考虑下面的例子：</X.P>
+            <X.HighlightBlock bgcolor="gray">
+                <X.P>
+                    每一个样本都形如{`$\\bm{X}=[x_1, x_2, \\dots, x_d]^T$`}
+                    ，其中的每维度都是二值变量，其分布可以由
+                    {`$\\bm{\\theta}=[\\theta_1, \\theta_2, \\dots, \\theta_d]^T$`}
+                    描述，即$s_i=1$的概率为$\theta_i$，相应地，$s_i=0$的概率为$1-\theta_i$。假设现在已经观察到样本
+                    {'$\\bm{\\chi}=\\{\\bm{X}_1, \\bm{X}_2, \\dots, \\bm{X}_n\\}$'}，希望估计参数{'$\\bm{\\theta}$'}。
+                </X.P>
+            </X.HighlightBlock>
         </X.BlogWrapper>
     );
 }
