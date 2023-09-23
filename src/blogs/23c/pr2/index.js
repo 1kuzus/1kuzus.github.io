91c050131dfbf23a35136957e81e018c5f7987be
1kuzus <revenge.1kuzus@outlook.com>
2023-09-23 23:21:10 +0800

    init

--- src/blogs/23c/pr2/index.js
+++ src/blogs/23c/pr2/index.js
@@ -33,7 +33,7 @@ export default function Blog() {
             </X.HighlightBlock>
             <X.P>观测到样本{'$\\bm{\\chi}$'}的概率为：</X.P>
             <X.Formula text="P(\bm{X}_1, \bm{X}_2, \dots, \bm{X}_n|\bm{\theta}) = \prod_{k=1}^n P(\bm{X}_i) = \prod_{k=1}^n \prod_{i=1}^d \theta_i^{x_{ki}}(1-\theta_i)^{1-x_{ki}}" />
-            <X.P>式子中的{`$x_{ki}$`}代表第`k`个样本的第`i`个分量，其值为`0`或`1`。</X.P>
+            <X.P>式子中的{`$x_{ki}$`}代表第$k$个样本的第$i$个分量，其值为`0`或`1`。</X.P>
             <X.P>取对数似然函数：</X.P>
             <X.Formula text="l(\bm{\theta}) = \ln P = \sum_{k=1}^n \sum_{i=1}^d [x_{ki}\ln\theta_i + (1-x_{ki})\ln(1-\theta_i)]" />
             <X.P>求偏导得：</X.P>
