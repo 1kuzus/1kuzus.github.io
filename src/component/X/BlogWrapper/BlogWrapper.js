c95fb3c7daa7dc35d3a07e8a3448138a63e2f74a
1kuzus <iikuzus@outlook.com>
2023-10-08 21:57:07 +0800

    init

--- src/component/X/BlogWrapper/BlogWrapper.js
+++ src/component/X/BlogWrapper/BlogWrapper.js
@@ -1,3 +1,4 @@
+import {useEffect} from 'react';
 import {SHA256Hash8} from '@/utils/SHA256Hash';
 import {H1, H2} from '../basics';
 import './BlogWrapper.css';
@@ -30,18 +31,30 @@ function BlogContent(props) {
 
 export default function BlogWrapper(props) {
     const {children} = props;
+    const titles = children.reduce((titles, current) => {
+        if (current.type === H1) return [...titles, {type: 'x-h1', text: current.props.children}];
+        else if (current.type === H2) return [...titles, {type: 'x-h2', text: current.props.children}];
+        else return titles;
+    }, []);
+    const scrollHandler = () => {
+        titles.map((title, index) => {
+            console.log(document.getElementById(SHA256Hash8(title.text)).offsetTop);
+            console.log(document.getElementById(SHA256Hash8(title.text)).clientTop);
+        });
+        console.log('---');
+    };
+
     oliIdx = 0;
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrollHandler);
+        return () => window.removeEventListener('scroll', scrollHandler);
+    }, []);
 
     return (
         <div className="x-blogwrapper">
             {children}
-            <BlogContent
-                titles={children.reduce((titles, current) => {
-                    if (current.type === H1) return [...titles, {type: 'x-h1', text: current.props.children}];
-                    else if (current.type === H2) return [...titles, {type: 'x-h2', text: current.props.children}];
-                    else return titles;
-                }, [])}
-            />
+            <BlogContent titles={titles} />
         </div>
     );
 }
