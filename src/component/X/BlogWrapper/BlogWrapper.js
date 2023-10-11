5d9c45763f94b38da39400ab86654420c9f4209e
1kuzus <revenge.1kuzus@outlook.com>
2023-10-11 11:35:09 +0800

    init

--- src/component/X/BlogWrapper/BlogWrapper.js
+++ src/component/X/BlogWrapper/BlogWrapper.js
@@ -19,17 +19,14 @@ function BlogContent(props) {
             {titles.map((title, index) => (
                 <div
                     key={index}
+                    className={`x-blogcontent-titletype-${title.type}${index === activeIdx ? ' active' : ''}`}
                     onClick={() => {
                         const titleElement = document.getElementById(SHA256Hash8(title.text));
                         document.documentElement.scrollTo({
                             top: titleElement.offsetTop - 64 - 16,
-                            behavior: 'smooth',
                         });
-                        // document.getElementById(SHA256Hash8(title.text)).scrollIntoView({behavior: 'smooth'});
                     }}
                 >
-                    {index === activeIdx ? '*' : ''}
-                    {title.type === 'x-h2' ? '. . .' : ''}
                     {title.text}
                 </div>
             ))}
@@ -52,11 +49,11 @@ export default function BlogWrapper(props) {
         const scrollHandler = () => {
             //找到最后一个自身offsetTop小于<html>根元素的scrollTop的title元素在titles数组中的位置。
             //也就是最后一个已经向上滚动出文本可视范围外(即元素顶部已经在header底边的上方)的标题的索引
-            //+64是header高度，+16是希望距离header底边有16px距离时就判定为进入下一主题。
+            //64是header高度，16是希望距离header底边有16px距离时就判定为进入下一块内容
             const lastIdx = titles
                 .map((title) => document.getElementById(SHA256Hash8(title.text)).offsetTop)
-                .findLastIndex((offset) => offset - 64 - 16 < document.documentElement.scrollTop);
-            //没有滚动时
+                .findLastIndex((offset) => offset - 64 - 16 + 1 < document.documentElement.scrollTop);
+            //lastIdx可能为-1，特殊处理
             setActiveIdx(Math.max(0, lastIdx));
         };
         window.addEventListener('scroll', scrollHandler);
