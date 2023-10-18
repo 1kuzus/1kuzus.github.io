8fc094fb8e607d855819907484add096f7fc2b8c
1kuzus <iikuzus@outlook.com>
2023-10-18 15:06:28 +0800

    init

--- src/component/X/Table/Table.js
+++ src/component/X/Table/Table.js
@@ -1,15 +1,22 @@
 import './Table.css';
 
 export default function Table(props) {
-    const {children} = props;
+    const {fasttext, children} = props;
 
-    if (typeof children === typeof '') {
-        const trs = children.split('---');
+    if (fasttext) {
+        const trs = fasttext.trim().split('\n');
         return (
             <table className="x-table" cellSpacing="0">
                 <tbody>
-                    {trs.map((tr, idx) => (
-                        <tr>{tr.split('|').map((td) => (idx ? <td>{td}</td> : <th>{td}</th>))}</tr>
+                    {trs.map((tr, tr_idx) => (
+                        <tr key={tr_idx}>
+                            {tr
+                                .trim()
+                                .split('|')
+                                .map((td, td_idx) =>
+                                    tr_idx ? <td key={td_idx}>{td}</td> : <th key={td_idx}>{td}</th>
+                                )}
+                        </tr>
                     ))}
                 </tbody>
             </table>
