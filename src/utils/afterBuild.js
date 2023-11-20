fb5707ba4e73a3024af0e728bb1b5d5e265aa76a
1kuzus <revenge.1kuzus@outlook.com>
2023-11-21 03:19:18 +0800

    init

--- src/utils/afterBuild.js
+++ src/utils/afterBuild.js
@@ -3,44 +3,37 @@ const path = require('path');
 const sitemap = require('sitemap');
 
 console.log('--- Running afterBuild.js ---');
-
 const routepathsAndFilepaths = fs
     .readFileSync(path.join('src', 'blogs', 'categories.js'), 'utf-8')
     .toString()
     .match(/import (.*?) from '(.*?)'/g)
     .map((i) => [i.split(' ')[1].toLowerCase(), i.split(' ')[3].replace(/'/g, '')]);
-
-// routepathsAndFilepaths.forEach(([routepath, filepath]) => {
-//     fs.mkdir(path.join('build', routepath), (err) => {
-//         if (err) throw err;
-//         const dasta = fs.readFileSync(path.join('src', 'blogs', filepath, 'index.js'), 'utf-8');
-//         console.log(dasta);
-//         fs.copyFile(path.join('build', 'index.html'), path.join('build', routepath, 'index.html'), (err) => {
-//             if (err) throw err;
-//             console.log(`copy index.html to ${routepath}\\index.html`);
-//         });
-//     });
-// });
-
 routepathsAndFilepaths.forEach(([routepath, filepath]) => {
-    let srcBlogCode = fs.readFileSync(path.join('src', 'blogs', filepath, 'index.js'), 'utf-8');
-    srcBlogCode = srcBlogCode
+    fs.mkdirSync(path.join('build', routepath));
+    console.log(`writing ${path.join('build', routepath, 'index.html')}`);
+    const rawBlogCode = fs.readFileSync(path.join('src', 'blogs', filepath, 'index.js'), 'utf-8');
+    const noScriptCode = rawBlogCode
         .match(/<X.BlogWrapper>(.*?)<\/X.BlogWrapper>/s)[0]
         .replace(/<X.BlogWrapper>(.*?)<\/X.BlogWrapper>/gs, '<div>$1</div>')
-        //删除
-        .replace(/<X.Divider \/>/gs, '')
-        //固定替换
-        .replace(/<X.Table(.*?)\/>/gs, '<div>[TABLE]</div>')
-        .replace(/<X.Table(.*?)>(.*?)<\/X.Table>/gs, '<div>[TABLE]</div>')
-        .replace(/<X.Image(.*?)\/>/gs, '<div>[IMAGE]</div>')
+        //固定替换或删除
         .replace(/<X.Br \/>/gs, '<br/>')
+        .replace(/<X.Divider \/>/gs, '')
+        .replace(/<X.Uli(.*?)>/gs, '')
+        .replace(/<\/X.Uli>/gs, '')
+        .replace(/<X.Oli(.*?)>/gs, '')
+        .replace(/<\/X.Oli>/gs, '')
+        .replace(/<X.Table(.*?)>(.*?)<\/X.Table>/gs, '<div>[TABLE]</div>')
+        .replace(/<X.Table(.*?)\/>/gs, '<div>[TABLE]</div>')
+        .replace(/<X.Image(.*?)\/>/gs, '<div>[IMAGE]</div>')
+        .replace(/<X.HighlightBlock(.*?)>/gs, '<div>')
+        .replace(/<\/X.HighlightBlock>/gs, '</div>')
+        .replace(/<X.CodeBlock(.*?)code={`(.*?)[^\\]`}(.*?)\/>/gs, '<div>[CODEBLOCK]</div>')
         //仅标签
         .replace(/<X.Title>(.*?)<\/X.Title>/gs, '<h1>$1</h1>')
         .replace(/<X.H1>(.*?)<\/X.H1>/gs, '<h2>$1</h2>')
-        .replace(/<X.H2>(.*?)<\/X.H2>/gs, '<h3>$1</h3>')
-        .replace(/<X.H3>(.*?)<\/X.H3>/gs, '<h4>$1</h4>')
-        .replace(/<X.Uli(.*?)>(.*?)<\/X.Uli>/gs, '$2')
-        .replace(/<X.Oli(.*?)>(.*?)<\/X.Oli>/gs, '$2')
+        .replace(/<X.H2(.*?)>(.*?)<\/X.H2>/gs, '<h3>$2</h3>')
+        .replace(/<X.H3(.*?)>(.*?)<\/X.H3>/gs, '<h4>$2</h4>')
+        .replace(/<X.FlexRow(.*?)>(.*?)<\/X.FlexRow>/gs, '<div>$2</div>')
         .replace(/<X.Formula\s*text="(.*?)"\s*\/>/gs, '$1')
         //段落
         .replace(
@@ -49,19 +42,42 @@ routepathsAndFilepaths.forEach(([routepath, filepath]) => {
                 '<p>' +
                 group2
                     .replace(/---/gs, '')
-                    .replace(/\\\\/g, '&#92;')
-                    .replace(/\\`/g, '&#96;')
-                    .replace(/\\\*/g, '&#42;')
-                    .replace(/\\@/g, '&#64;')
+                    .replace(/\\\\/g, '[backslash]')
+                    .replace(/\\`/g, '[backtick]')
+                    .replace(/\\\*/g, '[star]')
+                    .replace(/\\@/g, '[at]')
+                    .replace(/\\\$/g, '[dollar]')
                     .replace(/\\n/g, '<br/>')
                     .replace(/{`(.*?)`}/g, '$1')
                     .replace(/`(.*?)`/g, '$1')
                     .replace(/\*(.*?)\*/g, '$1')
                     .replace(/@(.*?)\[(.*?)\]@/g, '<a href="$2" target="_blank">$1</a>') +
                 '</p>'
-        );
-    console.log(srcBlogCode);
+        )
+        //删除其他标签
+        .replace(/<[A-Z](.*?)>(.*?)<\/[A-Z](.*?)>/gs, '')
+        .replace(/<[A-Z](.*?)>/gs, '')
+        //空格和换行
+        .replace(/ {1,}/gs, ' ')
+        .replace(/\n /gs, '')
+        .replace(/\n/gs, '');
+    const htmlCode = fs
+        .readFileSync(path.join('build', 'index.html'), 'utf-8')
+        .replace(/<div id="noscript"><\/div>/, `<noscript>${noScriptCode}</noscript>`);
+    fs.writeFileSync(path.join('build', routepath, 'index.html'), htmlCode);
 });
+
+console.log('--- Updating build\\index.html ---');
+const homepageLinks =
+    '<ul>' +
+    routepathsAndFilepaths
+        .map(([routepath, filepath]) => `<li><a href="${routepath}" target="_blank">${filepath.split('/')[2]}</a></li>`)
+        .join('') +
+    '</ul>';
+const htmlCode = fs
+    .readFileSync(path.join('build', 'index.html'), 'utf-8')
+    .replace(/<div id="noscript"><\/div>/, `<noscript>${homepageLinks}</noscript>`);
+fs.writeFileSync(path.join('build', 'index.html'), htmlCode);
 
 console.log('--- Writing sitemap.xml ---');
 const sitemapStream = new sitemap.SitemapStream({hostname: 'https://1kuzus.github.io'});
