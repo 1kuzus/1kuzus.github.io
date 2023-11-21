18a85590db464634276f0242f8e92f57bdd4ceb7
1kuzus <revenge.1kuzus@outlook.com>
2023-11-21 22:27:48 +0800

    init

--- src/component/X/Image/Image.js
+++ src/component/X/Image/Image.js
@@ -2,6 +2,7 @@ import './Image.css';
 
 export default function Image(props) {
     const {...rest} = props;
+    //todo: 做预览的时候记得加上禁止预览的选择
     return (
         <div className="x-img-wrapper">
             <img alt="img" {...rest}></img>
