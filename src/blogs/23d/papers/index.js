3e15701cbf361d54fe16ad9ad9f2aa7568cdc6ec
1kuzus <iikuzus@outlook.com>
2023-12-05 18:17:22 +0800

    init

--- src/blogs/23d/papers/index.js
+++ src/blogs/23d/papers/index.js
@@ -1,26 +1,77 @@
 import X from '@/component/X';
 
+function PaperSummary(props) {
+    const {title, time, link, topic, method, experiment, innovation, limitation, others} = props;
+
+    return (
+        <>
+            <X.H2 href={link}>{`${title} (${time})`}</X.H2>
+            <X.Table>
+                <tr>
+                    <th>文章的主题 / 要解决什么问题？</th>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <th>文章的核心方法 / 是如何做的？</th>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <th>做了什么实验，效果怎么样？</th>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <th>研究的创新点</th>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <th>有什么限制或可以改进的地方</th>
+                    <td>1</td>
+                </tr>
+            </X.Table>
+            <X.H3>其他</X.H3>
+        </>
+    );
+}
+
 export default function Blog() {
     return (
         <X.BlogWrapper>
             <X.Title>论文速记</X.Title>
-            <X.H3>Instance_Neural_Radiance_Field_ICCV_2023_paper</X.H3>
+            <X.H1>研究</X.H1>
+            <PaperSummary
+                title="NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis"
+                time="2020"
+                link="https://arxiv.org/pdf/2003.08934.pdf"
+            />
+            <X.H2>Instance Neural Radiance Field</X.H2>
             <X.Table>
                 <tr>
-                    <th>解决的是什么问题</th>
+                    <th>链接</th>
                     <td>1</td>
                 </tr>
                 <tr>
-                    <th>基于哪些研究</th>
+                    <th>文章的主题 / 要解决什么问题？</th>
                     <td>1</td>
                 </tr>
                 <tr>
-                    <th>有哪些限制</th>
+                    <th>文章的核心方法 / 是如何做的？</th>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <th>做了什么实验，效果怎么样？</th>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <th>研究的创新点</th>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <th>有什么限制或可以改进的地方</th>
                     <td>1</td>
                 </tr>
             </X.Table>
-            <X.H3>文章核心想法</X.H3>
-            <X.H3>其他笔记</X.H3>
+            <X.H3>其他</X.H3>
+            <X.H1>学习</X.H1>
         </X.BlogWrapper>
     );
 }
