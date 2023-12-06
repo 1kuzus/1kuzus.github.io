e64bba34a849518c01412fac006f2d33190087be
1kuzus <iikuzus@outlook.com>
2023-12-06 15:59:50 +0800

    init

--- src/blogs/23d/papers/index.js
+++ src/blogs/23d/papers/index.js
@@ -1,88 +1,100 @@
 import X from '@/component/X';
 
-// 文章要解决什么问题？
-// 文章的核心方法 / 是如何做的？
-// 做了什么实验，效果怎么样？
-// 研究的创新点
-// 有什么限制或可以改进的地方
 function PaperSummary(props) {
-    const {title, time, link, topic, method, experiment, innovation, limitation, others} = props;
+    const {topic, method, experiment, innovation, limitation} = props;
     return (
-        <>
-            <X.H2 href={link}>{`${title}${time && ' (' + time + ')'}`}</X.H2>
-            <X.HighlightBlock bgcolor="gray">
-                {X.Oli({reset: 0}) && false}
-                {topic && (
-                    <X.Oli>
-                        <X.P>*文章的主题 / 文章要解决什么问题？*</X.P>
-                        {topic}
-                    </X.Oli>
-                )}
+        <X.HighlightBlock bgcolor="gray">
+            {X.Oli({reset: 0}) && false}
+            {topic && (
                 <X.Oli>
-                    <X.P>文章的核心方法 / 具体是如何做的？</X.P>
+                    <X.P noMarginBottom>*文章的主题 / 文章要解决什么问题？*</X.P>
+                    {typeof topic === 'string' ? <X.P>{topic}</X.P> : topic}
                 </X.Oli>
+            )}
+            {method && (
                 <X.Oli>
-                    <X.P>做了什么实验，效果怎么样？</X.P>
+                    <X.P noMarginBottom>*文章的核心方法 / 具体是如何做的？*</X.P>
+                    {typeof method === 'string' ? <X.P>{method}</X.P> : method}
                 </X.Oli>
+            )}
+            {experiment && (
                 <X.Oli>
-                    <X.P>研究的创新点</X.P>
+                    <X.P noMarginBottom>*做了什么实验，效果怎么样？*</X.P>
+                    {typeof experiment === 'string' ? <X.P>{experiment}</X.P> : experiment}
                 </X.Oli>
+            )}
+            {innovation && (
                 <X.Oli>
-                    <X.P>有什么限制或可以改进的地方？</X.P>
+                    <X.P noMarginBottom>*研究的创新点*</X.P>
+                    {typeof innovation === 'string' ? <X.P>{innovation}</X.P> : innovation}
                 </X.Oli>
-            </X.HighlightBlock>
-            <X.HighlightBlock bgcolor="gray">
-                <X.H3>其他笔记</X.H3>
-            </X.HighlightBlock>
-        </>
+            )}
+            {limitation && (
+                <X.Oli>
+                    <X.P noMarginBottom>*有什么限制或可以改进的地方？*</X.P>
+                    {typeof limitation === 'string' ? <X.P>{limitation}</X.P> : limitation}
+                </X.Oli>
+            )}
+        </X.HighlightBlock>
     );
 }
+
+/************************************************
+ * 提问示例
+请分别回答下面五个问题：
+1.文章主要想解决什么问题？
+2.文章是如何做的，核心方法是什么？
+3.文章做了什么实验，效果怎么样？
+4.研究有什么创新点？
+5.研究有什么限制或可以改进的地方？
+
+ * 组件使用示例
+<X.H2 href="https://arxiv.org/pdf/2003.08934.pdf">
+NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis (2020)
+</X.H2>
+<PaperSummary
+    topic="文章主要解决了用5D空间表示3D合成"
+    method="文章主要解决了用5D空间表示3D合成"
+    experiment="文章主要解决了用5D空间表示3D合成"
+    innovation="文章主要解决了用5D空间表示3D合成"
+    limitation="文章主要解决了用5D空间表示3D合成"
+/>
+<X.HighlightBlock bgcolor="gray">
+    <X.H3>其他笔记</X.H3>
+</X.HighlightBlock>
+************************************************/
 
 export default function Blog({blogTitle}) {
     return (
         <X.BlogWrapper>
             <X.Title>{blogTitle}</X.Title>
             <X.H1>研究</X.H1>
+            <X.H2 href="https://arxiv.org/pdf/2003.08934.pdf">
+                NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis (2020)
+            </X.H2>
             <PaperSummary
-                title="NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis"
-                time="2020"
-                link="https://arxiv.org/pdf/2003.08934.pdf"
                 topic="文章主要解决了用5D空间表示3D合成"
+                method="文章主要解决了用5D空间表示3D合成"
+                experiment="文章主要解决了用5D空间表示3D合成"
+                innovation="文章主要解决了用5D空间表示3D合成"
+                limitation="文章主要解决了用5D空间表示3D合成"
             />
+            <X.HighlightBlock bgcolor="gray">
+                <X.H3>其他笔记</X.H3>
+            </X.HighlightBlock>
+            <X.H2 href="https://arxiv.org/pdf/2003.08934.pdf">
+                NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis (2020)
+            </X.H2>
             <PaperSummary
-                title="NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis"
-                time="2020"
-                link="https://arxiv.org/pdf/2003.08934.pdf"
-                topic="文章主要解决了用5D空间表示3D合成"
+                topic={<X.P>文章主要解决了用`5D`空间表示`3D`合成</X.P>}
+                method={
+                    <>
+                        <X.P>文章主要解决了用`5D`空间表`3D`合成</X.P>
+                        <X.P>文章主要解决了用`D`空间表示`3D`合成</X.P>
+                        <X.P>文章主要解决用`5D`空间表示`3D`合成</X.P>
+                    </>
+                }
             />
-            <X.H2>Instance Neural Radiance Field</X.H2>
-            <X.Table>
-                <tr>
-                    <th>链接</th>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <th>文章的主题 / 要解决什么问题？</th>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <th>文章的核心方法 / 是如何做的？</th>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <th>做了什么实验，效果怎么样？</th>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <th>研究的创新点</th>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <th>有什么限制或可以改进的地方</th>
-                    <td>1</td>
-                </tr>
-            </X.Table>
-            <X.H3>其他</X.H3>
             <X.H1>学习</X.H1>
         </X.BlogWrapper>
     );
