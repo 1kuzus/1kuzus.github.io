e50cc9a59278cb0857550473d1915f84dcc0879e
1kuzus <iikuzus@outlook.com>
2024-01-09 14:13:33 +0800

    update: 404 page

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -3,8 +3,11 @@ import './NotFound.css';
 export default function Homepage() {
     return (
         <div id="notfound">
-            <img alt="img" src={require('./cry.gif')} />
-            <div id="notfound-404">404</div>
+            <div id="notfound-404">
+                <span id="notfound-4l">4</span>
+                <img alt="img" src={require('./cry.gif')} />
+                <span id="notfound-4r">4</span>
+            </div>
             <div id="notfound-text">Page Not Found</div>
         </div>
     );
