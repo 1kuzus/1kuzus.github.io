41ae7277c4d32e53d0526f78f6b8f0f1c905e230
1kuzus <iikuzus@outlook.com>
2024-01-22 00:32:12 +0800

    feat: add Consolas_Latin_Subset font

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -1,11 +1,15 @@
+import {useLayoutEffect} from 'react';
 import './NotFound.css';
 
 export default function Homepage() {
+    useLayoutEffect(() => {
+        document.title = '找不到页面';
+    }, []);
     return (
         <div id="notfound">
             <div id="notfound-404">
                 <span id="notfound-4l">4</span>
-                <img alt="img" src={require('./cry.gif')} />
+                <img alt="0" src={require('./cry.gif')} />
                 <span id="notfound-4r">4</span>
             </div>
             <div id="notfound-text">Page Not Found</div>
