23af131721aaa092c2f5fbcaf85be6f220ba28dd
1kuzus <iikuzus@outlook.com>
2024-01-28 10:23:20 +0800

    fix: x-image max-width

--- src/component/X/Image/Image.js
+++ src/component/X/Image/Image.js
@@ -4,7 +4,7 @@ export default function Image(props) {
     const {invertInDarkTheme, ...rest} = props;
     //todo: 做预览的时候记得加上禁止预览的选择
     return (
-        <div className={`x-img-wrapper${invertInDarkTheme ? ' x-img-invert' : ''}`}>
+        <div className={`x-image-wrapper${invertInDarkTheme ? ' x-image-invert' : ''}`}>
             <img alt="img" {...rest} />
         </div>
     );
