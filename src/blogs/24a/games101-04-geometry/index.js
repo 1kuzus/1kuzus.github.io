511fd69bb0be4e484cf8d5ec924ea153c6a32958
1kuzus <iikuzus@outlook.com>
2024-01-31 21:54:06 +0800

    update blogs

--- src/blogs/24a/games101-04-geometry/index.js
+++ src/blogs/24a/games101-04-geometry/index.js
@@ -30,15 +30,61 @@ export default function Blog({blogTitle}) {
                 （交、并、差等）来定义新的几何体。
             </X.P>
             <X.Image src={require('./fig2.jpg')} width="100%" />
-            <X.H2>距离函数</X.H2>
-            <X.P>距离函数定义空间中一点到要描述的表面的最短距离。</X.P>
+            <X.H2>有符号距离函数 - SDF</X.H2>
+            <X.P>
+                有符号距离函数`(Signed Distance Function, SDF)`定义空间中一点到要描述的表面的最短距离，---
+                如果点在物体内部，则这个距离取负数。
+            </X.P>
+            <X.H3>合并和相交</X.H3>
+            <X.P>
+                通过对两个SDF函数的运算，得到一个新的SDF函数，再求出这个新SDF函数值为$0$的部分，即可得到组合后的图形。
+            </X.P>
+            <X.Image src={require('./fig3.png')} width="800px" />
+            <X.P>
+                要注意的是，这个结果并不是完全准确的，例如下图中的黄点的组合SDF值为两条黄色线段的最小值，但其准确值应为绿色线段的长度。---
+                出现这个问题的原因是，计算得到原图形SDF值的边界在新图形中已经不存在了。不过在应用中我们可以忽略这个小问题。
+            </X.P>
+            <X.Image src={require('./fig4.jpg')} width="500px" />
+            <X.H3>混合</X.H3>
+            <X.P>给两个图形的SDF函数加权相加可以实现混合`(blending)`效果。</X.P>
+            <X.Image src={require('./fig5.png')} width="100%" />
             <X.H2>分形</X.H2>
             <X.P>分形`(Fractals)`具有自相似性。</X.P>
             <X.FlexRow gap="32px">
-                <X.Image src={require('./fig3.jpg')} height="240px" invertInDarkTheme />
-                <X.Image src={require('./fig4.jpg')} height="240px" />
+                <X.Image src={require('./fig6.jpg')} height="240px" invertInDarkTheme />
+                <X.Image src={require('./fig7.jpg')} height="240px" />
             </X.FlexRow>
             <X.P>分形的变化频率非常高，在渲染时会引发强烈的走样。</X.P>
+            <X.H1>更多显式表示</X.H1>
+            <X.H2>点云</X.H2>
+            <X.P>点云是最简单的表示方法，用列表存储各个点的信息。只要采样足够密集，理论上可以表示任何几何体。</X.P>
+            <X.H2>多边形</X.H2>
+            <X.P>图形学中最广泛的应用通常是三角形面。</X.P>
+            <X.H3>Wavefront Object (*.obj) 文件简介</X.H3>
+            <X.P>
+                这是Wavefront科技开发的一种描述三维对象的文件格式，是一个文本文件，通常包含顶点`v`，法线`vn`，面`f`等定义信息；---
+                例如下面的代码表达了一个四面体：
+            </X.P>
+            <X.CodeBlock
+                language="none"
+                code={`
+                # 顶点
+                v  1.0  1.0  1.0
+                v -1.0 -1.0  1.0
+                v -1.0  1.0 -1.0
+                v  1.0 -1.0 -1.0
+
+                # 面
+                f 1 2 3
+                f 1 3 4
+                f 1 4 2
+                f 2 4 3
+                `}
+            />
+            <X.P>
+                `f`后面是顶点的索引。更详细的资料可以参考@维基百科[https://en.wikipedia.org/wiki/Wavefront_.obj_file]@。
+            </X.P>
+            <X.P>网站@ONLINE 3D VIEWER[https://3dviewer.net/]@可以在线查看各种格式定义的3D模型。</X.P>
         </X.BlogWrapper>
     );
 }
