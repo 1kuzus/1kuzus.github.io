d47f6d52653544faea0204c55bce3fc06fdf0097
1kuzus <revenge.1kuzus@outlook.com>
2024-02-01 23:38:56 +0800

    fix: merge branches

--- src/utils/randomString.js
+++
@@ -1,9 +0,0 @@
-const chars = '0123456789abcdefghijklmnopqrstuvwxyz';
-function randomStr(len) {
-    let str = '';
-    for (let i = 0; i < len; i++) {
-        str += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return str;
-}
-console.log(randomStr(4));
