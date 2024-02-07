08141d1bb89d2a08e3d3a51880f60e9c07dc5520
1kuzus <iikuzus@outlook.com>
2024-02-07 18:39:35 +0800

    update: content behavior

--- src/component/X/BlogWrapper/BlogWrapper.js
+++ src/component/X/BlogWrapper/BlogWrapper.js
@@ -35,18 +35,27 @@ export default function BlogWrapper(props) {
     const [titles, setTitles] = useState([]);
     oliIdx = 0;
     useLayoutEffect(() => {
-        document.documentElement.scrollTo({top: 0});
+        const container = document.documentElement;
+        container.scrollTo({top: 0});
         const titleNodes = Array.from(document.querySelectorAll('.x-blogwrapper>.x-h1,.x-blogwrapper>.x-h2'));
+        if (!titleNodes?.length) return;
         setTitles(titleNodes);
         const scrollHandler = () => {
-            if (!titleNodes) return;
-            //找到最后一个自身offsetTop小于<html>根元素的titleNode元素在titleNodes数组中的位置
-            //也就是最后一个已经向上滚动出文本可视范围外(即元素顶部已经在header底边的上方)的标题的索引
-            //64是header高度，32是希望距离header底边有32px距离时就判定为进入下一块内容
-            const lastIdx = titleNodes
-                .map((titleNode) => titleNode.offsetTop)
-                .findLastIndex((offset) => offset - 64 - 32 <= document.documentElement.scrollTop);
-            //lastIdx可能为-1，特殊处理
+            const titleNodesOffsetTop = titleNodes.map((titleNode) => titleNode.offsetTop - 96);
+            const containerScrollTop = Math.ceil(container.scrollTop);
+
+            // const line = (a, b, c, d, x) => ((d - b) / (c - a)) * (x - a) + b;
+            // const maxContainerScrollTop = container.scrollHeight - container.clientHeight;
+            // const ot_i_idx = titleNodesOffsetTop.findLastIndex((offset) => offset <= maxContainerScrollTop);
+            // const ot_i = ot_i_idx !== -1 ? titleNodesOffsetTop[ot_i_idx] : 0;
+            // const ot_n = titleNodesOffsetTop[titleNodes.length - 1];
+            // const threshold =
+            //     containerScrollTop < ot_i
+            //         ? containerScrollTop
+            //         : Math.max(containerScrollTop, line(ot_i, ot_i, maxContainerScrollTop, ot_n, containerScrollTop));
+
+            const threshold = containerScrollTop;
+            const lastIdx = titleNodesOffsetTop.findLastIndex((offset) => offset <= threshold);
             setActiveIdx(Math.max(0, lastIdx));
         };
         window.addEventListener('scroll', scrollHandler);
