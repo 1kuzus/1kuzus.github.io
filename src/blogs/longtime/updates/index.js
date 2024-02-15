7308f93d8ef4adc6824f932b6acdbe0a102c85f3
1kuzus <revenge.1kuzus@outlook.com>
2024-02-15 18:43:31 +0800

    update: responsive

--- src/blogs/longtime/updates/index.js
+++ src/blogs/longtime/updates/index.js
@@ -71,7 +71,7 @@ function UpdateLog() {
         {date: '2024-01-26', description: '使用语义化更佳的路由方案'},
         {date: '2024-01-27', description: '支持直接在列表项中写含有jsx表达式的`children`'},
         {date: '2024-02-01', description: '`HighlightBlock`组件支持`bgcolor="blue"`'},
-        {date: '2024-02-12', description: '不再使用`BlogWrapper`组件，改为`BlogLayout`\n响应式设计'},
+        {date: '2024-02-12', description: '不再使用`BlogWrapper`组件，改为`BlogLayout`\\n响应式设计'},
         {version: '1.1'},
     ];
     return (
