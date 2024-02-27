69bf12c530632b6c9858166b496aed341c157398
1kuzus <revenge.1kuzus@outlook.com>
2024-02-27 16:40:26 +0800

    fix: contents

--- src/component/Contents/Contents.js
+++ src/component/Contents/Contents.js
@@ -4,26 +4,22 @@ import './Contents.css';
 export default function Contents(props) {
     const {titleNodes} = props;
     const [activeIndex, setActiveIndex] = useState(0);
+    const getMappedOffsetTop = (titleNodes, container) => {
+        const titleNodesOffsetTop = titleNodes.map((titleNode) => titleNode.offsetTop - 80);
+        // return titleNodesOffsetTop; //旧版
+        const maxContainerScrollTop = container.scrollHeight - container.clientHeight;
+        const ot_i = titleNodesOffsetTop.findLast((offset) => offset <= maxContainerScrollTop) || 0;
+        const ot_n = titleNodesOffsetTop[titleNodes.length - 1];
+        const line = (a, b, c, d, x) => ((d - b) / (c - a)) * (x - a) + b;
+        return titleNodesOffsetTop.map((ot) => (ot <= ot_i ? ot : line(ot_i, ot_i, ot_n, maxContainerScrollTop, ot)));
+    };
     useLayoutEffect(() => {
-        const container = document.documentElement;
         const scrollHandler = () => {
-            const titleNodesOffsetTop = titleNodes.map((titleNode) => titleNode.offsetTop - 80);
-            const containerScrollTop = Math.ceil(container.scrollTop);
-
-            // 新版
-            const line = (a, b, c, d, x) => ((d - b) / (c - a)) * (x - a) + b;
-            const maxContainerScrollTop = container.scrollHeight - container.clientHeight;
-            const ot_i = titleNodesOffsetTop.findLast((offset) => offset <= maxContainerScrollTop) || 0;
-            const ot_n = titleNodesOffsetTop[titleNodes.length - 1];
-            const threshold =
-                containerScrollTop < ot_i || ot_i === ot_n
-                    ? containerScrollTop
-                    : line(ot_i, ot_i, maxContainerScrollTop, ot_n, containerScrollTop);
-            // 旧版
-            // const threshold = containerScrollTop;
-
-            const lastIdx = titleNodesOffsetTop.findLastIndex((offset) => offset <= threshold);
-            setActiveIndex(Math.max(0, lastIdx));
+            const mappedOffsetTop = getMappedOffsetTop(titleNodes, document.documentElement);
+            const lastIdx = mappedOffsetTop.findLastIndex(
+                (offset) => offset <= Math.ceil(document.documentElement.scrollTop)
+            );
+            setActiveIndex(lastIdx);
         };
         window.addEventListener('scroll', scrollHandler);
         return () => window.removeEventListener('scroll', scrollHandler);
@@ -45,7 +41,10 @@ export default function Contents(props) {
                         <li
                             key={index}
                             className={`titletype-${type}${index === activeIndex ? ' active' : ''}`}
-                            onClick={() => document.documentElement.scrollTo({top: titleNode.offsetTop - 80})}
+                            onClick={() => {
+                                const mappedOffsetTop = getMappedOffsetTop(titleNodes, document.documentElement);
+                                document.documentElement.scrollTo({top: Math.ceil(mappedOffsetTop[index])});
+                            }}
                         >
                             {text}
                         </li>
