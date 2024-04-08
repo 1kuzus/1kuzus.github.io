0cb9a548e17ece28101c5234088b128295af785e
1kuzus <revenge.1kuzus@outlook.com>
2024-04-08 11:11:33 +0800

    migrate to Next.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+const nextSitemapConfig = {
+    siteUrl: 'https://1kuzus.github.io',
+    changefreq: null,
+    priority: null,
+    generateIndexSitemap: false,
+    outDir: 'out',
+};
+
+module.exports = nextSitemapConfig;
