1b4e7a8f8f4e45e431db084eeec728f4214b9471
1kuzus <iikuzus@outlook.com>
2024-04-10 20:01:52 +0800

    migrate to Next.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,21 @@ if (!localStorage.getItem('theme')) localStorage.setItem('theme', 'light');
 document.documentElement.setAttribute('class', localStorage.getItem('theme'));
 `;
 
+/*
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme !== 'light' && currentTheme !== 'dark') {
+    localStorage.setItem('theme', 'light');
+}
+if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.setAttribute('class', 'dark');
+} else {
+    document.documentElement.setAttribute('class', 'light');
+}
+*/
+//初始化色彩主题
+const scriptTheme =
+    "const a=z=>h.getItem(z),b=(y,z)=>h.setItem(y,z),c=(y,z)=>document.documentElement.setAttribute(y,z),d='theme',e='dark',f='light',g='class',h=localStorage;a(d)!==e&&a(d)!==f&&b(d,f);a(d)===e?c(g,e):c(g,f);";
+
 export default function RootLayout({children}) {
     return (
         <html lang="zh-CN" suppressHydrationWarning>
@@ -23,23 +38,7 @@ export default function RootLayout({children}) {
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-45BYSZ6WPY" />
                 <script async src="https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js" />
                 <script dangerouslySetInnerHTML={{__html: scriptGA}} />
-                <script
-                    /*
-                    const currentTheme = localStorage.getItem('theme');
-                    if (currentTheme !== 'light' && currentTheme !== 'dark') {
-                        localStorage.setItem('theme', 'light');
-                    }
-                    if (localStorage.getItem('theme') === 'dark') {
-                        document.documentElement.setAttribute('class', 'dark');
-                    } else {
-                        document.documentElement.setAttribute('class', 'light');
-                    }
-                     */
-                    //初始化色彩主题
-                    dangerouslySetInnerHTML={{
-                        __html: "const a=z=>h.getItem(z),b=(y,z)=>h.setItem(y,z),c=(y,z)=>document.documentElement.setAttribute(y,z),d='theme',e='dark',f='light',g='class',h=localStorage;a(d)!==e&&a(d)!==f&&b(d,f);a(d)===e?c(g,e):c(g,f);",
-                    }}
-                />
+                <script dangerouslySetInnerHTML={{__html: scriptTheme}} />
             </head>
             <body>
                 <GlobalProvider>
