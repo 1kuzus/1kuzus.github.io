7db46aec6c853ffcfc76954e51dcf5319c8c968e
1kuzus <revenge.1kuzus@outlook.com>
2024-04-23 13:00:49 +0800

    update blogs

--- src/app/(blogs)/24b/by-questions/page.js
+++ src/app/(blogs)/24b/by-questions/page.js
@@ -28,11 +28,16 @@ export default function Blog() {
             <X.H1>机器学习与深度学习</X.H1>
             <X.Oli reset={1}>
                 <X.P noMarginBottom>正负样本不均衡怎么办？</X.P>
-                <X.P>
-                    答：从定义来说，矩阵的秩等于行向量组的秩，也就是行向量组中线性无关的向量的最大数目，对于列向量组同样成立，矩阵的行秩等于列秩。---
-                    从几何意义来说，矩阵的秩是列空间或行空间的维度，也是线性变换后的空间维度。
-                </X.P>
+                <X.P>答：一个简单的做法是可以通过上采样和下采样，但下采样会导致数据的丢失，上采样可能导致过拟合；</X.P>
             </X.Oli>
+            <X.CodeBlock
+                language="diff-cpp"
+                code={`
+                #include <fdddddddddddddddddddducy> 
+                - int x=hello;
+                + double s=helsssssslo;
+                `}
+            />
         </>
     );
 }
