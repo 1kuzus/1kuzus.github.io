ba3929f9899ccf30f26609b46ccf7613fb8cdb05
1kuzus <iikuzus@outlook.com>
2024-04-30 03:18:10 +0800

    update blogs

--- src/component/X/Paragraph/Paragraph.js
+++ src/component/X/Paragraph/Paragraph.js
@@ -12,6 +12,12 @@ export default function P(props) {
     let htmlContent = Array.isArray(children) ? children.join('') : children;
     htmlContent = htmlContent
         .replace(/--- /g, '')
+        .replace(/\$(.*?)\$/g, (_, group1) => {
+            return Katex.renderToString(group1, {
+                output: 'html',
+                strict: false,
+            });
+        })
         .replace(/\\\\/g, '&#92;')
         .replace(/\\`/g, '&#96;')
         .replace(/\\\*/g, '&#42;')
@@ -20,13 +26,8 @@ export default function P(props) {
         .replace(/\\n/g, '<br/>')
         .replace(/`(.*?)`/g, '<code class="x-inline-highlight">$1</code>')
         .replace(/\*(.*?)\*/g, '<span class="x-inline-strong">$1</span>')
-        .replace(/@(.*?)\[(.*?)\]@/g, '<a href="$2" target="_blank" rel="noreferrer" class="x-inline-link">$1</a>')
-        .replace(/\$(.*?)\$/g, (_, group1) => {
-            return Katex.renderToString(group1, {
-                output: 'html',
-                strict: false,
-            });
-        });
+        .replace(/@(.*?)\[(.*?)\]@/g, '<a href="$2" target="_blank" rel="noreferrer" class="x-inline-link">$1</a>');
+
     return (
         <p
             className={`x-p${withMarginTop ? ' with-margin-top' : ''}${noMarginBottom ? ' no-margin-bottom' : ''}`}
