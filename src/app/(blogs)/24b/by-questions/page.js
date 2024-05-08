bdbf993046bb0b93dcbe36524daec20462078820
1kuzus <revenge.1kuzus@outlook.com>
2024-05-09 00:08:41 +0800

    update blogs

--- src/app/(blogs)/24b/by-questions/page.js
+++ src/app/(blogs)/24b/by-questions/page.js
@@ -287,14 +287,14 @@ export default function Blog() {
                     <X.P>模式识别非参数估计：直方图，k近邻，Parzen窗。</X.P>
                 </X.HighlightBlock>
             </X.Oli>
-            <X.H1>算法与数据结构</X.H1>
+            {/* <X.H1>算法与数据结构</X.H1>
             <X.H1>计算机网络</X.H1>
             <X.H1>编程语言</X.H1>
             <X.H1>机器学习与深度学习</X.H1>
             <X.Oli reset={1}>
                 <X.P>正负样本不均衡怎么办？</X.P>
                 <X.P>一个简单的做法是可以通过上采样和下采样，但下采样会导致数据的丢失，上采样可能导致过拟合；</X.P>
-            </X.Oli>
+            </X.Oli> */}
             <X.H1>杂题</X.H1>
             <X.Oli reset={1}>
                 <X.P>
