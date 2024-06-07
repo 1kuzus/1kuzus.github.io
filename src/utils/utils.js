d91c4e3333727827d3851a510bc879e1aad9fb94
1kuzus <iikuzus@outlook.com>
2024-06-07 21:08:59 +0800

    chore

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,4 @@
 export function isStringOrStringArray(x) {
-    console.log(x)
     if (typeof x === 'string') return true;
     if (Array.isArray(x)) {
         return x.every((i) => typeof i === 'string');
