a0b79282941fec3a97ae4583836c16b9825c2a4e
1kuzus <iikuzus@outlook.com>
2024-06-09 13:43:53 +0800

    chore

--- src/component/X/CodeBlock/CodeBlock.js
+++ src/component/X/CodeBlock/CodeBlock.js
@@ -1,15 +1,17 @@
 import Prism from 'prismjs';
 import 'prismjs/components/prism-c';
 import 'prismjs/components/prism-cpp';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-tsx';
+import 'prismjs/components/prism-php';
 import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-diff';
 import 'prismjs/components/prism-python';
 import 'prismjs/components/prism-markdown';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-typescript';
-import 'prismjs/components/prism-jsx';
-import 'prismjs/components/prism-tsx';
-import 'prismjs/components/prism-diff';
-import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-markup-templating';
 import './CodeBlock.css';
 
 export default function CodeBlock(props) {
