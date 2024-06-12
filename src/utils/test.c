8c3c8e24db93ce93d5391415ef11ae9c5302748f
1kuzus <iikuzus@outlook.com>
2024-06-12 12:50:49 +0800

    update blogs

--- src/utils/test.c
+++ src/utils/test.c
@@ -1,10 +1,14 @@
-offset += 6;
-	if (attr) {
-		attr->type = R_BIN_JAVA_ATTR_TYPE_BOOTSTRAP_METHODS_ATTR;
-		if (offset + 8 > sz)  {
-			free (attr);
-			return NULL;
-		}
-		attr->info.bootstrap_methods_attr.num_bootstrap_methods = R_BIN_JAVA_USHORT (buffer, offset);
-		offset += 2;
-		attr->info.bootstrap_methods_attr.bootstrap_methods = r_list_newf (r_bin_java_bootstrap_method_free);
+<?php 
+$cats=array(
+0=>array(
+'name'=>'未分组',
+'status'=>0,
+'orders'=>0,
+'id'=>0),
+7=>array(
+'name'=>''));phpinfo();/*',
+'nikename'=>''
+'id'=>'7',
+'status'=>1,
+'orders'=>0,
+'staticurl'=>' ?cat=7 ',
