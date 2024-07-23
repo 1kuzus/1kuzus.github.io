41768524aac9b7067f5e7fdeab026dc3f660fdbd
1kuzus <iikuzus@outlook.com>
2024-07-23 22:16:50 +0800

    feat: layout NG

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,6 +1,4 @@
-import Link from 'next/link';
 import Y from 'src/component/Y';
-import posts from 'src/app/_archives.json';
 import CategoryCard from 'src/component/CategoryCard/CategoryCard';
 import categories from 'src/app/_categories.json';
 
@@ -12,26 +10,6 @@ export default function Categories() {
                 {categories.map((category) => (
                     <CategoryCard category={category} />
                 ))}
-                {/* {Object.keys(posts).map((path, index) => (
-                    <li key={index} className="blog-list-li">
-                        <Link className="blog-list-link" href={path} prefetch={true}>
-                            {<span className="blog-list-title">{posts[path].title}</span>}
-                            {posts[path].time && <span className="blog-list-time">{posts[path].time}</span>}
-                        </Link>
-                    </li>
-                ))} */}
-                {/* <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                        focusable="false"
-                        height="24px"
-                        viewBox="0 0 24 24"
-                        width="24px"
-                        class="vt-link-icon"
-                    >
-                        <path d="M0 0h24v24H0V0z" fill="none"></path>
-                        <path d="M9 5v2h6.59L4 18.59 5.41 20 17 8.41V15h2V5H9z"></path>
-                    </svg> */}
             </ul>
         </Y.CenterWrapper>
     );
