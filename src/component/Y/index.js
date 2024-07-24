cc1360a4b7604c309a8aae0b7dd0662852c44890
1kuzus <iikuzus@outlook.com>
2024-07-24 16:47:17 +0800

    feat: layout NG

--- src/component/Y/index.js
+++ src/component/Y/index.js
@@ -1,10 +1,11 @@
 import CenterWrapper from './CenterWrapper/CenterWrapper';
-import {H1} from './Headers';
+import {H1, H2} from './Headers';
 import Link from './Link/Link';
 
 const Y = {
     CenterWrapper,
     H1,
+    H2,
     Link,
 };
 
