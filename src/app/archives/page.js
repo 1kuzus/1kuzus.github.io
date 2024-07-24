f6f91878340cc5275044b91943295a2792d850d5
1kuzus <iikuzus@outlook.com>
2024-07-24 16:47:27 +0800

    feat: layout NG

--- src/app/archives/page.js
+++ src/app/archives/page.js
@@ -1,12 +1,39 @@
 import Y from 'src/component/Y';
 import posts from 'src/app/_archives.json';
+import './page.css';
 
 export default function Archives() {
+    const paths = Object.keys(posts);
+    const postsByYears = {};
+    for (let path of paths) {
+        const y = posts[path].time?.split('-')[0] || '0';
+        if (!postsByYears[y]) postsByYears[y] = [path];
+        else postsByYears[y].push(path);
+    }
+    const years = Object.keys(postsByYears).sort((a, b) => b - a);
     return (
         <Y.CenterWrapper>
             <Y.H1>Archives.</Y.H1>
+            <Y.H2>共{paths.length}篇文章。</Y.H2>
+            {years.map((year, index) => (
+                <div key={index}>
+                    <Y.H2>{+year || '长期维护'}</Y.H2>
+                    <ul>
+                        {postsByYears[year].map((path, index) => (
+                            <li key={index}>
+                                <Y.Link href={path} prefetch={true}>
+                                    {posts[path].time && (
+                                        <span className="archives-post-time">{posts[path].time.slice(5)}</span>
+                                    )}
+                                    {<span className="archives-post-title">{posts[path].title}</span>}
+                                </Y.Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
             {/* <h1>{Object.keys(posts).length}</h1> */}
-            <ul>
+            {/* <ul>
                 {Object.keys(posts).map((path, index) => (
                     <li key={index}>
                         <Y.Link href={path} prefetch={true}>
@@ -17,7 +44,7 @@ export default function Archives() {
                         </Y.Link>
                     </li>
                 ))}
-            </ul>
+            </ul> */}
         </Y.CenterWrapper>
     );
 }
