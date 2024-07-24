5277c27f3a2d00eb29be0ef852f366f332c201ad
1kuzus <revenge.1kuzus@outlook.com>
2024-07-24 16:47:17 +0800

    feat: layout NG

--- src/component/Y/Headers/index.js
+++ src/component/Y/Headers/index.js
@@ -4,3 +4,8 @@ export function H1(props) {
     const {children} = props;
     return <h2 className="y-h1">{children}</h2>;
 }
+
+export function H2(props) {
+    const {children} = props;
+    return <h3 className="y-h2">{children}</h3>;
+}
