7193fc97e991e8614e79305556d91ae9df0c7063
1kuzus <iikuzus@outlook.com>
2024-07-27 13:22:13 +0800

    feat: layout NG

--- src/app/(posts)/longtime/updates/Version.js
+++ src/app/(posts)/longtime/updates/Version.js
@@ -1,12 +1,13 @@
 import X from 'src/component/X';
 import './Version.css';
 
-export default function Version() {
+export default function Version(props) {
+    const {version} = props;
     return (
         <X.HighlightBlock>
             <X.FlexRow justifyContent="space-around">
                 <X.Image id="cheer-gif" src="cheer.gif" />
-                <div id="current-version">Version 3.1</div>
+                <div id="current-version">Version {version}</div>
                 <X.Image id="dance-gif" src="dance.gif" />
             </X.FlexRow>
         </X.HighlightBlock>
