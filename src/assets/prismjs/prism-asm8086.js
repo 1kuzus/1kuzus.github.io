059fe6e546f17577320e276a8f63f94e42b2e5fe
1kuzus <revenge.1kuzus@outlook.com>
2024-08-15 21:13:15 +0800

    update posts

--- src/assets/prismjs/prism-asm8086.js
+++ src/assets/prismjs/prism-asm8086.js
@@ -9,7 +9,7 @@ Prism.languages.asm8086 = {
     function:
         /\b(?:jmp|mov|push|pop|xchg|in|out|xlat|lea|lds|les|lahf|sahf|add|adc|inc|aaa|daa|sub|sbb|dec|neg|cmp|aas|das|mul|imul|aam|div|idiv|aad|cbw|cwd|not|and|or|xor|test|shl|sal|shr|sar|rol|ror|rcl|rcr|rep|repe|repz|repne|repnz|movs|movsb|movsw|cmps|cmpsb|cmpsw|scas|scasb|scasw|lods|lodsb|lodsw|stos|stosb|stosw|call|ret|retf|je|jz|jl|jnz|jle|jg|jge|jb|jc|jnae|jbe|ja|js|jns|jp|jpe|jnp|jpo|loop|loope|loopz|loopne|loopnz|jcxz|clc|cld|cli|clv|cmc|stc|std|sti|stv|hlt|wait|nop|esc|lock|seg|int)\b/i,
     keyword:
-        /\b(?:assume|end|db|dw|dd|dq|dt|equ|org|proc|endp|segment|ends|public|extern|global|section|bits|times|resb|resw|resd|resq|byte|word|dword|qword|align)\b/i,
+        /\b(?:assume|end|db|dw|dd|dq|dt|dup|equ|org|proc|endp|segment|ends|public|extern|global|section|bits|times|resb|resw|resd|resq|byte|word|dword|qword|ptr|align)\b/i,
     register: {
         pattern: /\b(?:ax|bx|cx|dx|si|di|sp|bp|ah|bh|ch|dh|al|bl|cl|dl|cs|ds|es|fs|gs|ss|ip|flags)\b/i,
         alias: 'variable',
