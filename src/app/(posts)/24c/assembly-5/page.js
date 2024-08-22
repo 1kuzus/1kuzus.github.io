d1da7ad978943b323d6366df2bd710e5ab45d34d
1kuzus <iikuzus@outlook.com>
2024-08-22 23:43:06 +0800

    fix: Margins

--- src/app/(posts)/24c/assembly-5/page.js
+++ src/app/(posts)/24c/assembly-5/page.js
@@ -14,9 +14,14 @@ export default function Post() {
 
             <X.P>用来存储相关指令的某些执行结果 ; 用来为CPU执行相关指令提供行为依据 ; 用来控制CPU的相关工作方式</X.P>
 
-            <X.Image src="fig1.jpg" width="100%" />
-            <X.Image src="fig2.jpg" width="100%" />
+            <X.Image src="fig1.jpg" width="600px" invertInDarkTheme a />
+            <X.Image src="fig2.jpg" width="600px" invertInDarkTheme />
+            {/* todo:black white */}
             <X.H2>ZF(Zero Flag)零标志</X.H2>
+
+            <X.HighlightBlock>
+                <X.P>123</X.P>
+            </X.HighlightBlock>
         </>
     );
 }
