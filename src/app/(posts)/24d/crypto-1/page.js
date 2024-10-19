659a46960eb32ed8d633512137bdc59c0ba7d603
1kuzus <revenge.1kuzus@outlook.com>
2024-10-19 16:40:36 +0800

    update posts

--- src/app/(posts)/24d/crypto-1/page.js
+++ src/app/(posts)/24d/crypto-1/page.js
@@ -133,7 +133,201 @@ export default function Post() {
             <X.H2>先压缩还是先加密？</X.H2>
             <X.P>如题，假如想在传输数据时同时应用压缩和加密，则应该*先压缩后加密*。因为压缩本来就是利用信息冗余，而如果先加密，生成的密文近似随机，则很难再有压缩空间。</X.P>
             <X.H2>编程作业</X.H2>
-            
+            <X.P>使用同一个流密码密钥多次加密存在安全隐患。给出了以下`11`个密文：</X.P>
+            <X.CodeBlock
+                language="python"
+                code={`
+                ciphers = [
+                    "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e",
+                    "234c02ecbbfbafa3ed18510abd11fa724fcda2018a1a8342cf064bbde548b12b07df44ba7191d9606ef4081ffde5ad46a5069d9f7f543bedb9c861bf29c7e205132eda9382b0bc2c5c4b45f919cf3a9f1cb74151f6d551f4480c82b2cb24cc5b028aa76eb7b4ab24171ab3cdadb8356f",
+                    "32510ba9a7b2bba9b8005d43a304b5714cc0bb0c8a34884dd91304b8ad40b62b07df44ba6e9d8a2368e51d04e0e7b207b70b9b8261112bacb6c866a232dfe257527dc29398f5f3251a0d47e503c66e935de81230b59b7afb5f41afa8d661cb",
+                    "32510ba9aab2a8a4fd06414fb517b5605cc0aa0dc91a8908c2064ba8ad5ea06a029056f47a8ad3306ef5021eafe1ac01a81197847a5c68a1b78769a37bc8f4575432c198ccb4ef63590256e305cd3a9544ee4160ead45aef520489e7da7d835402bca670bda8eb775200b8dabbba246b130f040d8ec6447e2c767f3d30ed81ea2e4c1404e1315a1010e7229be6636aaa",
+                    "3f561ba9adb4b6ebec54424ba317b564418fac0dd35f8c08d31a1fe9e24fe56808c213f17c81d9607cee021dafe1e001b21ade877a5e68bea88d61b93ac5ee0d562e8e9582f5ef375f0a4ae20ed86e935de81230b59b73fb4302cd95d770c65b40aaa065f2a5e33a5a0bb5dcaba43722130f042f8ec85b7c2070",
+                    "32510bfbacfbb9befd54415da243e1695ecabd58c519cd4bd2061bbde24eb76a19d84aba34d8de287be84d07e7e9a30ee714979c7e1123a8bd9822a33ecaf512472e8e8f8db3f9635c1949e640c621854eba0d79eccf52ff111284b4cc61d11902aebc66f2b2e436434eacc0aba938220b084800c2ca4e693522643573b2c4ce35050b0cf774201f0fe52ac9f26d71b6cf61a711cc229f77ace7aa88a2f19983122b11be87a59c355d25f8e4",
+                    "32510bfbacfbb9befd54415da243e1695ecabd58c519cd4bd90f1fa6ea5ba47b01c909ba7696cf606ef40c04afe1ac0aa8148dd066592ded9f8774b529c7ea125d298e8883f5e9305f4b44f915cb2bd05af51373fd9b4af511039fa2d96f83414aaaf261bda2e97b170fb5cce2a53e675c154c0d9681596934777e2275b381ce2e40582afe67650b13e72287ff2270abcf73bb028932836fbdecfecee0a3b894473c1bbeb6b4913a536ce4f9b13f1efff71ea313c8661dd9a4ce",
+                    "315c4eeaa8b5f8bffd11155ea506b56041c6a00c8a08854dd21a4bbde54ce56801d943ba708b8a3574f40c00fff9e00fa1439fd0654327a3bfc860b92f89ee04132ecb9298f5fd2d5e4b45e40ecc3b9d59e9417df7c95bba410e9aa2ca24c5474da2f276baa3ac325918b2daada43d6712150441c2e04f6565517f317da9d3",
+                    "271946f9bbb2aeadec111841a81abc300ecaa01bd8069d5cc91005e9fe4aad6e04d513e96d99de2569bc5e50eeeca709b50a8a987f4264edb6896fb537d0a716132ddc938fb0f836480e06ed0fcd6e9759f40462f9cf57f4564186a2c1778f1543efa270bda5e933421cbe88a4a52222190f471e9bd15f652b653b7071aec59a2705081ffe72651d08f822c9ed6d76e48b63ab15d0208573a7eef027",
+                    "466d06ece998b7a2fb1d464fed2ced7641ddaa3cc31c9941cf110abbf409ed39598005b3399ccfafb61d0315fca0a314be138a9f32503bedac8067f03adbf3575c3b8edc9ba7f537530541ab0f9f3cd04ff50d66f1d559ba520e89a2cb2a83"
+                ]
+
+                # target:
+                target = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"
+                `}
+            />
+            <X.P>已知这些密文都是使用同一个密钥进行的加密，加密的方法是直接异或（就是一次性密码本），这里密钥足够长，对于每条明文，只取与明文等长的密钥前缀进行加密。（后面的讨论中对于串长度不同的情况，也默认以最短的为准。）目标是求出最后一条密文`target`对应的明文。</X.P>
+            <X.P>当两条消息使用相同的密钥$k$进行加密时，由于$c_1=m_1 \oplus k$，$c_2=m_2 \oplus k$，则有：</X.P>
+            <X.Formula text="c_1 \oplus c_2 = m_1 \oplus m_2" />
+            <X.P>考虑两个ASCII字符异或的结果，ASCII字符存在一些特性：</X.P>
+            <X.Uli>英文字母的ASCII码值大于`64`，也就是第`7`位（倒数第`2`高位）一定是`1`；</X.Uli>
+            <X.Uli>同一英文字母，大小写的ASCII码值差`32`（翻转第`6`位）；</X.Uli>
+            <X.Uli>空格的ASCII码值刚好是`32`，因此字符异或空格的结果相当于翻转大小写；</X.Uli>
+            <X.Uli>字符异或字符的值小于`64`，字符异或空格的值大于`64`。</X.Uli>
+            <X.P>根据上述规律，思想是将`target`与前`10`条密文分别异或，如果结果大于`64`，则有可能泄露`target`在这一位上的字符。不过消息明文中可能含有特殊符号或者非英文字符干扰结果，例如ASCII码值小于`64`的英文标点与字符异或后也会结果大于`64`，因此要根据统计结果综合判断。</X.P>
+            <X.P>首先为了方便编程，先把字符串密文逐字节转成列表：</X.P>
+            <X.CodeBlock
+                language="python"
+                code={`
+                def cipher_to_byte_list(cipher):
+                    return [int(cipher[i:i + 2], 16) for i in range(0, len(cipher), 2)]
+
+
+                ciphers = [cipher_to_byte_list(c) for c in ciphers]
+                target = cipher_to_byte_list(target)
+                `}
+            />
+            <X.P>然后编写`leak`函数，统计每一位`i`上，`target[i]`与`cipher[i]`异或的结果，大于等于`64`则保存下来（保存的时候再次异或`32`，记录原始的字符）：</X.P>
+            <X.CodeBlock
+                language="python"
+                code={`
+                stats = [[] for _ in range(len(target))]
+
+
+                def leak(cipher):
+                    for i in range(len(target)):  # target是最短的，这里直接用len(target)即可
+                        if target[i] ^ cipher[i] >= 64:
+                            stats[i].append(cipher[i] ^ target[i] ^ ord(' '))
+
+
+                for cipher in ciphers:
+                    leak(cipher)
+                `}
+            />
+            <X.P>当异或结果大于等于`64`时有以下几种情况：</X.P>
+            <X.Uli>`target[i]`是字符，`cipher[i]`是空格，此时通常`stats[i]`中会有`1`~`3`个相同的值，这个值就是`target[i]`的明文字符；</X.Uli>
+            <X.Uli>`target[i]`是空格，`cipher[i]`是字符，通常只有这种情况下`stats[i]`的长度比较长，并且值各不相同，因为同一个位置出现字符的概率显然更大；</X.Uli>
+            <X.Uli>出现了特殊字符，此时可能会干扰结果，因此通过取众数来决定结果；</X.Uli>
+            <X.CodeBlock
+                language="python"
+                code={`
+                def mode(ls):  # 求数组ls的众数
+                    return max(set(ls), key=ls.count)
+
+
+                message = ""
+                for stats_ch in stats:
+                    if len(stats_ch) > 5:
+                        message += "."
+                    else:
+                        m = mode(stats_ch)
+                        if len(stats_ch) == 1 or stats_ch.count(m) > 1:
+                            message += chr(m)
+                        else:
+                            ch = "?"
+                            for c in stats_ch:
+                                if 65 <= c <= 90 or 97 <= c <= 122:
+                                    ch = chr(c)
+                            message += ch
+
+                print(message)  # The.secuet.message.is..Whtn.usinw.asstream.cipher..never.use.the.key.more.than.once
+                `}
+            />
+            <X.P>最后还需要手动修正一些字符，最后猜测的明文为：</X.P>
+            <X.CodeBlock language="text" code="The secret message is: When using a stream cipher, never use the key more than once" />
+            <X.Divider />
+            <X.P>到这里已经做完了，但还可以通过恢复的明文进一步反求出`ciphers`中的原始消息，观察为何求出的`target`的部分位置会有错误：</X.P>
+            <X.CodeBlock
+                language="python"
+                code={`
+                message = "The secret message is: When using a stream cipher, never use the key more than once"  # 个别字符需要根据上下文推测一下
+
+                # 反推一下其他十个密文，长度只取到len(target)
+                print("-" * 10, "The other ten messages are:", "-" * 10)
+                message = [ord(ch) for ch in message]
+                key = [message[i] ^ target[i] for i in range(len(target))]
+                for cipher in ciphers:
+                    text = [cipher[i] ^ key[i] for i in range(len(key))]
+                    print("".join([chr(ch) for ch in text]))
+                `}
+            />
+            <X.P>结果为：</X.P>
+            <X.CodeBlock
+                language="text"
+                code={`
+                ---------- The other ten messages are: ----------
+                We can factor the number 15 with quantum computers. We can also factor the number 1
+                Euler would probably enjoy that now his theorem becomes a corner stone of crypto - 
+                The nice thing about Keeyloq is now we cryptographers can drive a lot of fancy cars
+                The ciphertext produced by a weak encryption algorithm looks as good as ciphertext 
+                You don't want to buy a set of car keys from a guy who specializes in stealing cars
+                There are two types of cryptography - that which will keep secrets safe from your l
+                There are two types of cyptography: one that allows the Government to use brute for
+                We can see the point where the chip is unhappy if a wrong bit is sent and consumes 
+                A (private-key)  encryption scheme states 3 algorithms, namely a procedure for gene
+                The Concise OxfordDictionary (2006) deï¬nes crypto as the art of  writing o r sol
+                `}
+            />
+            <X.P>完整代码：</X.P>
+            <X.CodeBlock
+                language="python"
+                code={`
+                ciphers = [
+                    "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e",
+                    "234c02ecbbfbafa3ed18510abd11fa724fcda2018a1a8342cf064bbde548b12b07df44ba7191d9606ef4081ffde5ad46a5069d9f7f543bedb9c861bf29c7e205132eda9382b0bc2c5c4b45f919cf3a9f1cb74151f6d551f4480c82b2cb24cc5b028aa76eb7b4ab24171ab3cdadb8356f",
+                    "32510ba9a7b2bba9b8005d43a304b5714cc0bb0c8a34884dd91304b8ad40b62b07df44ba6e9d8a2368e51d04e0e7b207b70b9b8261112bacb6c866a232dfe257527dc29398f5f3251a0d47e503c66e935de81230b59b7afb5f41afa8d661cb",
+                    "32510ba9aab2a8a4fd06414fb517b5605cc0aa0dc91a8908c2064ba8ad5ea06a029056f47a8ad3306ef5021eafe1ac01a81197847a5c68a1b78769a37bc8f4575432c198ccb4ef63590256e305cd3a9544ee4160ead45aef520489e7da7d835402bca670bda8eb775200b8dabbba246b130f040d8ec6447e2c767f3d30ed81ea2e4c1404e1315a1010e7229be6636aaa",
+                    "3f561ba9adb4b6ebec54424ba317b564418fac0dd35f8c08d31a1fe9e24fe56808c213f17c81d9607cee021dafe1e001b21ade877a5e68bea88d61b93ac5ee0d562e8e9582f5ef375f0a4ae20ed86e935de81230b59b73fb4302cd95d770c65b40aaa065f2a5e33a5a0bb5dcaba43722130f042f8ec85b7c2070",
+                    "32510bfbacfbb9befd54415da243e1695ecabd58c519cd4bd2061bbde24eb76a19d84aba34d8de287be84d07e7e9a30ee714979c7e1123a8bd9822a33ecaf512472e8e8f8db3f9635c1949e640c621854eba0d79eccf52ff111284b4cc61d11902aebc66f2b2e436434eacc0aba938220b084800c2ca4e693522643573b2c4ce35050b0cf774201f0fe52ac9f26d71b6cf61a711cc229f77ace7aa88a2f19983122b11be87a59c355d25f8e4",
+                    "32510bfbacfbb9befd54415da243e1695ecabd58c519cd4bd90f1fa6ea5ba47b01c909ba7696cf606ef40c04afe1ac0aa8148dd066592ded9f8774b529c7ea125d298e8883f5e9305f4b44f915cb2bd05af51373fd9b4af511039fa2d96f83414aaaf261bda2e97b170fb5cce2a53e675c154c0d9681596934777e2275b381ce2e40582afe67650b13e72287ff2270abcf73bb028932836fbdecfecee0a3b894473c1bbeb6b4913a536ce4f9b13f1efff71ea313c8661dd9a4ce",
+                    "315c4eeaa8b5f8bffd11155ea506b56041c6a00c8a08854dd21a4bbde54ce56801d943ba708b8a3574f40c00fff9e00fa1439fd0654327a3bfc860b92f89ee04132ecb9298f5fd2d5e4b45e40ecc3b9d59e9417df7c95bba410e9aa2ca24c5474da2f276baa3ac325918b2daada43d6712150441c2e04f6565517f317da9d3",
+                    "271946f9bbb2aeadec111841a81abc300ecaa01bd8069d5cc91005e9fe4aad6e04d513e96d99de2569bc5e50eeeca709b50a8a987f4264edb6896fb537d0a716132ddc938fb0f836480e06ed0fcd6e9759f40462f9cf57f4564186a2c1778f1543efa270bda5e933421cbe88a4a52222190f471e9bd15f652b653b7071aec59a2705081ffe72651d08f822c9ed6d76e48b63ab15d0208573a7eef027",
+                    "466d06ece998b7a2fb1d464fed2ced7641ddaa3cc31c9941cf110abbf409ed39598005b3399ccfafb61d0315fca0a314be138a9f32503bedac8067f03adbf3575c3b8edc9ba7f537530541ab0f9f3cd04ff50d66f1d559ba520e89a2cb2a83"
+                ]
+
+                # target:
+                target = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"
+
+
+                def cipher_to_byte_list(cipher):
+                    return [int(cipher[i:i + 2], 16) for i in range(0, len(cipher), 2)]
+
+
+                ciphers = [cipher_to_byte_list(c) for c in ciphers]
+                target = cipher_to_byte_list(target)
+
+                stats = [[] for _ in range(len(target))]
+
+
+                def leak(cipher):
+                    for i in range(len(target)):  # target是最短的，这里直接用len(target)即可
+                        if target[i] ^ cipher[i] >= 64:
+                            stats[i].append(cipher[i] ^ target[i] ^ ord(' '))
+
+
+                for cipher in ciphers:
+                    leak(cipher)
+
+
+                def mode(ls):  # 求数组ls的众数
+                    return max(set(ls), key=ls.count)
+
+
+                message = ""
+                for stats_ch in stats:
+                    if len(stats_ch) > 5:
+                        message += "."
+                    else:
+                        m = mode(stats_ch)
+                        if len(stats_ch) == 1 or stats_ch.count(m) > 1:
+                            message += chr(m)
+                        else:
+                            ch = "?"
+                            for c in stats_ch:
+                                if 65 <= c <= 90 or 97 <= c <= 122:
+                                    ch = chr(c)
+                            message += ch
+
+                print(message)  # The.secuet.message.is..Whtn.usinw.asstream.cipher..never.use.the.key.more.than.once
+                message = "The secret message is: When using a stream cipher, never use the key more than once"  # 个别字符需要根据上下文推测一下
+
+                # 反推一下其他十个密文，长度只取到len(target)
+                print("-" * 10, "The other ten messages are:", "-" * 10)
+                message = [ord(ch) for ch in message]
+                key = [message[i] ^ target[i] for i in range(len(target))]
+                for cipher in ciphers:
+                    text = [cipher[i] ^ key[i] for i in range(len(key))]
+                    print("".join([chr(ch) for ch in text]))
+                `}
+            />
         </>
     );
 }
