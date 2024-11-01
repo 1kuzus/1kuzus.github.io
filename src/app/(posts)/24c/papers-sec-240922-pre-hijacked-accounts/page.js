b7486747be5d652e5cbe78399009eb20fb17c9ea
1kuzus <revenge.1kuzus@outlook.com>
2024-11-01 21:38:25 +0800

    update posts

--- src/app/(posts)/24c/papers-sec-240922-pre-hijacked-accounts/page.js
+++ src/app/(posts)/24c/papers-sec-240922-pre-hijacked-accounts/page.js
@@ -11,6 +11,7 @@ export default function Post() {
             <X.Title>{metas[path].title}</X.Title>
             <X.P>相关链接：</X.P>
             <X.Uli>@Pre-hijacked accounts: An Empirical Study of Security Failures in User Account Creation on the Web[https://www.usenix.org/system/files/sec22-sudhodanan.pdf]@</X.Uli>
+            <X.Uli>@演讲 - YouTube[https://www.youtube.com/watch?v=USl-4h-g908]@</X.Uli>
             <X.HighlightBlock background="blue">
                 <X.H3>笔记</X.H3>
                 <X.H3>IdP</X.H3>
@@ -32,7 +33,7 @@ export default function Post() {
             <X.Uli>目标用户想创建账户时意外发现已经存在，并且无法登录（因为密码是攻击者设置的），此时会选择重置密码；</X.Uli>
             <X.Uli>如果重置密码时服务商没有使所有活跃`session`失效，攻击者只要一直维持先前的`session`就可以持续访问目标用户账户。</X.Uli>
             <X.H2>Trojan Identifier Attack</X.H2>
-            <X.P>todo</X.P>
+            <X.P>像是前两种的结合：首先用目标用户的邮箱地址创建账号，然后与攻击者的IdP账号关联；当用户重置密码时攻击者仍然可以通过联合身份登录。</X.P>
             <X.H2>Unexpired Email Change Attack</X.H2>
             <X.P>更改电子邮件地址时，服务会向新电子邮件地址发送确认链接或验证码来确认更改者对邮箱的所有权；然而如果有效期过长（例如持续几天）可能会带来风险：</X.P>
             <X.Uli>假设攻击者用目标用户的邮箱地址和攻击者选择的密码能够成功创建一个账户；</X.Uli>
@@ -40,7 +41,7 @@ export default function Post() {
             <X.Uli>目标用户想创建账户时意外发现已经存在（因为攻击者没有确认更改，此时绑定的仍然是受害者邮箱），并且无法登录，此时会选择重置密码；</X.Uli>
             <X.Uli>攻击者稍后确认更改邮箱地址，并且可以用自己的邮箱来做重置密码的认证，这时目标用户就会丢失对此账户的访问权限。</X.Uli>
             <X.H2>Non-verifying IdP Attack</X.H2>
-            <X.P>todo</X.P>
+            <X.P>像是第一种攻击的镜像版本：攻击者通过联合方式注册（在攻击者的IdP上使用目标用户的邮箱），而用户通过普通方式注册，从而导致用户无感知的账户合并。</X.P>
         </>
     );
 }
