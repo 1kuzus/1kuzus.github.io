d59992e30107fe647b5351ce2504c4044609b398
1kuzus <revenge.1kuzus@outlook.com>
2024-11-25 20:32:48 +0800

    update posts

--- src/app/(posts)/longtime/wp-portswigger/page.js
+++ src/app/(posts)/longtime/wp-portswigger/page.js
@@ -12,6 +12,11 @@ export default function Post() {
             <X.H1>SQL injection</X.H1>
             <X.H2>Ap: SQL injection vulnerability allowing login bypass</X.H2>
             <X.P>直接用用户名`administrator' --`；或用户名`administrator`，密码`1' or 1=1 --`。</X.P>
+            <X.H1>Cross-site scripting (XSS)</X.H1>
+            <X.H2>Ap: Reflected XSS into HTML context with nothing encoded</X.H2>
+            <X.P>反射型XSS，搜索{'`<script>alert(0)</script>`'}。</X.P>
+            <X.H2>Ap: Stored XSS into HTML context with nothing encoded</X.H2>
+            <X.P>存储型XSS，评论{'`<script>alert(0)</script>`'}。</X.P>
             <X.H1>Path traversal</X.H1>
             <X.H2>Ap: File path traversal, simple case</X.H2>
             <X.P>随便检查一个图片，地址为`/image?filename=23.jpg`，改为`/txt?filename=../../../etc/passwd`即可。</X.P>
