4674b17d72e96fb87e3e7e80425b1beafa95e2fd
1kuzus <revenge.1kuzus@outlook.com>
2024-12-19 04:30:09 +0800

    feat: firebase

--- src/firebase/views.js
+++ src/firebase/views.js
@@ -3,17 +3,17 @@ import {ref, get, set, update, increment} from 'firebase/database';
 
 export async function getViews(path, isDev) {
     const refPath = isDev ? 'test-dev' : path.replace(/\//g, '_');
-    const viewsRef = ref(database, refPath);
-    const snapshot = await get(viewsRef);
-    if (!snapshot.exists()) {
-        await set(viewsRef, {views: 0});
+    const viewsRef = ref(database, refPath + '/views');
+    const viewsSnapshot = await get(viewsRef);
+    if (!viewsSnapshot.exists()) {
+        await set(viewsRef, 0);
         return 1;
     }
-    return snapshot.val().views + 1;
+    return viewsSnapshot.val() + 1;
 }
 
 export function increaseViews(path, isDev) {
     const refPath = isDev ? 'test-dev' : path.replace(/\//g, '_');
-    const viewsRef = ref(database, refPath);
-    return update(viewsRef, {views: increment(1)});
+    const pathRef = ref(database, refPath);
+    return update(pathRef, {views: increment(1)});
 }
