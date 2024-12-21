1262cbdfda53c8dee0b7b43f3a10b2b77d6f0631
1kuzus <revenge.1kuzus@outlook.com>
2024-12-21 12:34:26 +0800

    feat: firebase

--- src/component/ViewsCount/ViewsCount.js
+++ src/component/ViewsCount/ViewsCount.js
@@ -1,6 +1,6 @@
 'use client';
 import {useEffect, useState} from 'react';
-import {increaseViews, getViews} from 'src/firebase/views';
+import {increaseViews, getViews, viewsOnValue} from 'src/firebase/views';
 import './ViewsCount.css';
 
 export default function ViewsCount(props) {
@@ -10,10 +10,12 @@ export default function ViewsCount(props) {
     const isHomepage = path === 'total';
     useEffect(() => {
         getViews(path, isDev).then((count) => {
-            increaseViews(path, isDev);
-            isHomepage && increaseViews('total', isDev);
             setViewsCount(count);
+            increaseViews(path, isDev);
+            if (!isHomepage) increaseViews('total', isDev);
         });
+        // 主页动态监听总数
+        if (isHomepage) return viewsOnValue('total', isDev, (views) => setViewsCount(views));
     }, []);
     return (
         <div className="views-count">
--- src/firebase/views.js
+++ src/firebase/views.js
@@ -1,5 +1,5 @@
 import database from '.';
-import {ref, get, set, update, increment} from 'firebase/database';
+import {ref, get, set, update, increment, onValue} from 'firebase/database';
 
 export async function getViews(path, isDev) {
     const refPath = isDev ? 'test-dev' : path.replace(/\//g, '_');
@@ -16,4 +16,15 @@ export function increaseViews(path, isDev) {
     const refPath = isDev ? 'test-dev' : path.replace(/\//g, '_');
     const pathRef = ref(database, refPath);
     update(pathRef, {views: increment(1)});
+}
+
+export function viewsOnValue(path, isDev, callback) {
+    const refPath = isDev ? 'test-dev' : path.replace(/\//g, '_');
+    const viewsRef = ref(database, refPath + '/views');
+    // 返回值为取消订阅函数
+    return onValue(viewsRef, (viewsSnapshot) => {
+        const views = viewsSnapshot.val();
+        console.log('onvalue', views);
+        callback(views);
+    });
 }
