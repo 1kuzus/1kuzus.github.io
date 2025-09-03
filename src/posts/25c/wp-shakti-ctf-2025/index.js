b2a3476c49447bf63ba827f4f661c9748d591b17
1kuzus <revenge.1kuzus@outlook.com>
2025-09-03 14:56:49 +0800

    update posts

--- src/posts/25c/wp-shakti-ctf-2025/index.js
+++ src/posts/25c/wp-shakti-ctf-2025/index.js
@@ -17,38 +17,33 @@ export default function Post() {
             <X.CodeBlock
                 language="js"
                 code={String.raw`
-                const base = Module.findBaseAddress("havana32.exe");
+                const base = Module.findBaseAddress('havana32.exe');
 
                 const fn_read = base.add(0x6a40); // read buffer
                 const var_buffer = base.add(0x474c0); // buffer
                 Interceptor.replace(
-                fn_read,
-                new NativeCallback(
-                    function (a1, a2, a3) {
-                    console.log(
-                        "fn_read called, arg1=" + a1 + ", (buffer=" + var_buffer + ")"
-                    );
-                    const input = "[INPUT]";
-                    Memory.writeAnsiString(var_buffer, input);
-                    console.log(
-                        "Data written to var_buffer: " + var_buffer.readAnsiString(),
-                        input.length
-                    );
-                    return 1;
-                    },
-                    "int",
-                    ["pointer", "int", "int"]
-                )
+                    fn_read,
+                    new NativeCallback(
+                        function (a1, a2, a3) {
+                            console.log('fn_read called, arg1=' + a1 + ', (buffer=' + var_buffer + ')');
+                            const input = '[INPUT]';
+                            Memory.writeAnsiString(var_buffer, input);
+                            console.log('Data written to var_buffer: ' + var_buffer.readAnsiString(), input.length);
+                            return 1;
+                        },
+                        'int',
+                        ['pointer', 'int', 'int']
+                    )
                 );
 
                 const fn_vm_main = base.add(0x1300);
                 const var_matched = base.add(0x474b8); // matched count
                 Interceptor.attach(fn_vm_main, {
-                onLeave: function () {
-                    const matched = Memory.readU32(var_matched);
-                    console.log("matched:", matched);
-                    send({ matched: matched });
-                },
+                    onLeave: function () {
+                        const matched = Memory.readU32(var_matched);
+                        console.log('matched:', matched);
+                        send({matched: matched});
+                    },
                 });
                 `}
             />
