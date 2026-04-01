91abf0d8f2edd0a2cfbc516ebc79d8c6fd293e06
1kuzus <revenge.1kuzus@outlook.com>
2026-04-01 22:48:38 +0800

    Updates

---
+++ files/miniapp-lib/android.js
@@ -0,0 +1,129 @@
+;if (this.setTimeout && this.clearTimeout && this.setInterval && this.clearInterval && this.NativeGlobal) {
+    this.NativeGlobal.setTimeout = this.setTimeout;
+    this.NativeGlobal.clearTimeout = this.clearTimeout;
+    this.NativeGlobal.setInterval = this.setInterval;
+    this.NativeGlobal.clearInterval = this.clearInterval;
+};
+
+var WeixinJSCore = (function(global) {
+    var _WeixinJSCore = global.WeixinJSCore
+    if (!_WeixinJSCore) {
+        return undefined;
+    }
+
+    var __invokeHandler__ = _WeixinJSCore.invokeHandler
+    var __invokeHandler2__ = _WeixinJSCore.invokeHandler2
+    if (global.NativeGlobal && global.NativeGlobal.invokeHandler) {
+        __invokeHandler2__ = global.NativeGlobal.invokeHandler
+    } else if (global.workerInvokeJsApi) {
+        __invokeHandler2__ = global.workerInvokeJsApi
+    }
+
+    var ret = {};
+    ret.publishHandler = function(event, data, dst) {
+        _WeixinJSCore.publishHandler(event, data, dst)
+    }
+
+    ret.invokeHandler = function(api, args, callbackId, privateArgs) {
+        if (__invokeHandler2__) {
+            privateArgs = privateArgs || ""
+            if (typeof privateArgs !== 'string') {
+                privateArgs = JSON.stringify(privateArgs)
+            }
+            return __invokeHandler2__(api, args, callbackId, privateArgs)
+        } else {
+            return __invokeHandler__(api, args, callbackId)
+        }
+    }
+    if (global.workerInvokeJsApi) {
+        // 改为每次都查找 WeixinJSCore.invokeHandler，保证 node 的 hook 能生效
+        global.workerInvokeJsApi = function(api, args, callbackId, privateArgs) {
+            return ret.invokeHandler(api, args, callbackId, privateArgs)
+        }
+    }
+
+    return ret
+})(this);
+
+var WeixinJSCoreAndroid = (function(global) {
+    if (typeof global.WeixinJSCoreAndroid !== 'undefined') {
+        return global.WeixinJSCoreAndroid
+    }
+
+    var ret = {};
+    ret.invokeCallbackHandler = function(callbackId, data, ext) {
+        if (typeof WeixinJSBridge !== 'undefined') {
+            if (typeof data === 'string') {
+                try {
+                    data = JSON.parse(data)
+                } catch (e) {
+                    console.e('hy: fail not compatible data' + data)
+                }
+            }
+//            console.log('invokeCallbackHandler ' + JSON.stringify(data))
+            if (typeof ext === 'string' && ext.length > 0) {
+                try {
+                    ext = JSON.parse(ext)
+                } catch(e) {}
+            }
+            WeixinJSBridge.invokeCallbackHandler(callbackId, data, ext);
+        } else {
+            console.error('WeixinJSCoreAndroid::invokeCallbackHandler, WeixinJSBridge==undefined, callbackId==' + callbackId)
+        }
+    };
+    ret.subscribeHandler = function(event, data, src, ext) {
+        if ('__native_custom_event__wxConfig_inject' === event) {
+            data = JSON.parse(data)
+//            global.__wxIndexPage = data.__wxIndexPage
+//            delete data.__wxIndexPage
+            if (typeof global.__wxConfig === 'undefined') {
+                global.__wxConfig = {}
+            }
+            if (global.NativeGlobal !== undefined) {
+                global.NativeGlobal.hasInitializedWxConfig = true;
+            }
+            Object.assign(global.__wxConfig, data)
+            return
+        }
+
+        if (typeof WeixinJSBridge !== 'undefined') {
+//            console.log('subscribeHandler event:' + (typeof event) + ":" + event + ", data:" + (typeof data) + ":" + data + ", src:" + (typeof src) + ":" + src + ", ext:" + (typeof ext) + ":" + ext)
+
+            if (typeof data === 'string' && data.length > 0) {
+                try {
+                    data = JSON.parse(data)
+                } catch(e) {}
+            }
+            if (typeof src === 'string') {
+                src = Number(src)
+            }
+            if (typeof ext === 'string') {
+                ext = JSON.parse(ext)
+            }
+//            if (event === 'onError') {
+//                console.error('subscribeHandler onError ' + data)
+//            }
+//            console.log('subscribeHandler impl event:' + (typeof event) + ":" + event + ", data:" + (typeof data) + ":" + data + ", src:" + (typeof src) + ":" + src + ", ext:" + (typeof ext) + ":" + ext)
+            WeixinJSBridge.subscribeHandler(event, data, src, ext);
+        } else {
+            console.error('WeixinJSCoreAndroid::subscribeHandler, WeixinJSBridge==undefined, event==' + event)
+        }
+    };
+
+    return ret;
+})(this);
+
+// 直接注入的JsApi不支持多态，需要手动处理
+if (typeof this.WeixinWorker !== 'undefined') {
+    var createWorker = this.WeixinWorker.create;
+    var createWithParams = this.WeixinWorker.createWithParams;
+    if (createWorker && createWithParams) {
+        this.WeixinWorker.create = function(path, params) {
+            if (!params) {
+                return createWorker(path);
+            } else {
+                return createWithParams(path, JSON.stringify(params));
+            }
+        }
+    }
+};
