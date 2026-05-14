6978191726201f6f8dea9f2759ba3ed631de4436
1kuzus <revenge.1kuzus@outlook.com>
2026-05-14 21:57:29 +0800

    chore

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ import './not-found.css';
 export default function NotFound() {
     return (
         <div id="notfound">
-            <img alt="img" src="/images/cry.gif" />
+            <img alt="img" src="/cry.gif" />
             <code id="notfound-404">404</code>
             <code id="notfound-text">Page Not Found</code>
         </div>
