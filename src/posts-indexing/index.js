4d566499dfd97b5129406bff7f13b6e77a51cd72
1kuzus <revenge.1kuzus@outlook.com>
2026-08-15 03:02:22 +0800

    chore

--- src/posts-indexing/index.js
+++ src/posts-indexing/index.js
@@ -1,5 +1,5 @@
 import archives from './archives.json';
-import categories from './categories.json';
+import rawCategories from './categories.json';
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -8,9 +8,10 @@ let activeArchives = archives;
 if (!isDev) activeArchives = Object.fromEntries(Object.entries(archives).filter(([_, post]) => !post.inactive));
 
 // 无论开发/线上环境，均从categories的原分类中移除inactive文章
-categories.forEach((category) => {
-    category.posts = category.posts.filter((post) => !archives[post].inactive);
-});
+const categories = rawCategories.map((category) => ({
+    ...category,
+    posts: category.posts.filter((post) => !archives[post].inactive),
+}));
 
 // 开发环境，将inactive文章添加到一个新的分类中
 if (isDev) {
