c1c9287aa6cfff443951162c1b7dc5df7901db53
1kuzus <revenge.1kuzus@outlook.com>
2026-08-15 01:33:43 +0800

    fix: image bundle

--- src/component/X/Image/Image.js
+++ src/component/X/Image/Image.js
@@ -3,13 +3,15 @@ import NextImage from 'next/image';
 import {usePathname} from 'next/navigation';
 import './Image.css';
 
+const postImages = require.context('../../../posts', true, /\.(png|jpe?g|gif|webp|svg)$/i);
+
 export default function Image(props) {
     const {src, alt, width, themeAdaptive} = props;
     const pathname = usePathname();
     return (
         <NextImage
             className={`x-image${themeAdaptive ? ' x-image-invert' : ''}`}
-            src={require('src/posts' + pathname + src)}
+            src={postImages('.' + pathname + src)}
             alt={alt || 'image'}
             style={{width: width, height: 'auto'}}
         />
